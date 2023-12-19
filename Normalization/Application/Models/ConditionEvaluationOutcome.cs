3cefbe38f9231896d831fe16675b86025984256c
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2023-12-19 14:53:23 -0600

    Cond Trsfrm generic impl

---
+++ Normalization/Application/Models/ConditionEvaluationOutcome.cs
@@ -0,0 +1,11 @@
+﻿using LantanaGroup.Link.Normalization.Domain.Entities;
+
+namespace LantanaGroup.Link.Normalization.Application.Models;
+
+public class ConditionEvaluationOutcome
+{
+    public string Element { get; set; }
+    public string ElementValue { get; set; }
+    public ConditionElement ConditionElement { get; set; }
+    public bool ConditionMet { get; set; }
+}
