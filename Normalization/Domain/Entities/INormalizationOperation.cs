742580623d6b3b7095301e23db68257a39b82537
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2024-01-04 17:08:45 -0600

    Implement Period Date Fixer

--- Normalization/Domain/Entities/INormalizationOperation.cs
+++ Normalization/Domain/Entities/INormalizationOperation.cs
@@ -3,9 +3,10 @@ using System.Text.Json.Serialization;
 
 namespace LantanaGroup.Link.Normalization.Domain.Entities;
 
-[BsonKnownTypes(typeof(CopyLocationIdentifierToTypeOperation), typeof(CopyElementOperation), typeof(ConceptMapOperation), typeof(ConditionalTransformationOperation))]
+[BsonKnownTypes(typeof(CopyLocationIdentifierToTypeOperation), typeof(CopyElementOperation), typeof(ConceptMapOperation), typeof(ConditionalTransformationOperation), typeof(PeriodDateFixerOperation))]
 [JsonDerivedType(typeof(ConceptMapOperation), nameof(ConceptMapOperation))]
 [JsonDerivedType(typeof(CopyElementOperation), nameof(CopyElementOperation))]
 [JsonDerivedType(typeof(CopyLocationIdentifierToTypeOperation), nameof(CopyLocationIdentifierToTypeOperation))]
 [JsonDerivedType(typeof(ConditionalTransformationOperation), nameof(ConditionalTransformationOperation))]
+[JsonDerivedType(typeof(PeriodDateFixerOperation), nameof(PeriodDateFixerOperation))]
 public abstract class INormalizationOperation { }
