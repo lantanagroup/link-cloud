3e5a85f3560e37a30fd1a3bf56c0c06bf3b4796b
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2024-02-27 13:04:27 -0800

    Renaming test projects from `UnitTests` to `Tests` Removing unused projects, per Dan

--- PatientBundlePerMeasure/Application/Models/BundleEvalRequestedValue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LantanaGroup.Link.PatientBundlePerMeasure.Application.Models
-{
-    public partial class BundleEvalRequestedValue
-    {
-        public string Bundle { get; set; }
-        public ScheduledReport ScheduledReport { get; set; }
-    }
-}
