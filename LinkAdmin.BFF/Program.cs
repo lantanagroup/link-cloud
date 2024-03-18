21f79be7366ac14734e214c851ef40fcdbb985f3
Adam Phillips <adam.phillips@lantanagroup.com>
2024-03-18 12:59:59 -0400

    Initial project setup

---
+++ LinkAdmin.BFF/Program.cs
@@ -0,0 +1,68 @@
+using Azure.Identity;
+using LantanaGroup.Link.LinkAdmin.BFF.Settings;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//load external configuration source if specified
+var externalConfigurationSource = builder.Configuration.GetSection(LinkAdminConstants.AppSettingsSectionNames.ExternalConfigurationSource).Get<string>();
+if (!string.IsNullOrEmpty(externalConfigurationSource))
+{
+    switch (externalConfigurationSource)
+    {
+        case ("AzureAppConfiguration"):
+            builder.Configuration.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(builder.Configuration.GetConnectionString("AzureAppConfiguration"))
+                    // Load configuration values with no label
+                    .Select("Link:AdminBFF*", LabelFilter.Null)
+                    // Override with any configuration values specific to current hosting env
+                    .Select("Link:AdminBFF*", builder.Environment.EnvironmentName);
+
+                options.ConfigureKeyVault(kv =>
+                {
+                    kv.SetCredential(new DefaultAzureCredential());
+                });
+
+            });
+            break;
+    }
+}
+
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+
+app.Run();
+
---
+++ LinkAdmin.BFF/Settings/LinkAdminConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace LantanaGroup.Link.LinkAdmin.BFF.Settings
+{
+    public class LinkAdminConstants
+    {
+        public static class AppSettingsSectionNames
+        {
+            public const string ExternalConfigurationSource = "Link:AdminBFF:ExternalConfigurationSource";
+            public const string ServiceInformation = "Link:AdminBFF:ServiceInformation";
+            public const string IdentityProvider = "Link:AdminBFF:IdentityProviderConfig";
+            public const string Telemetry = "Link:AdminBFF:TelemetryConfig";
+            public const string Serilog = "Link:AdminBFF:Logging:Serilog";
+            public const string EnableSwagger = "Link:AdminBFF:EnableSwagger";
+        }
+
+        public static class AuditLoggingIds
+        {
+            public const int RequestRecieved = 1000;
+            public const int RequestRecievedWarning = 1001;
+            public const int RequestRecievedException = 1002;
+        }
+    }
+}
