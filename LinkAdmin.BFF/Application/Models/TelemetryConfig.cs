a958e19af61f89b56feab89c13cddb980eaa58c4
Adam Phillips <adam.phillips@lantanagroup.com>
2024-03-18 13:24:27 -0400

    Add open telemetry service extension

---
+++ LinkAdmin.BFF/Application/Models/TelemetryConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace LantanaGroup.Link.LinkAdmin.BFF.Application.Models
+{
+    public class TelemetryConfig
+    {
+        public bool EnableTracing { get; set; } = true;
+        public bool EnableMetrics { get; set; } = true;
+        public bool EnableRuntimeInstrumentation { get; set; } = false;
+        public string TraceExporterEndpoint { get; set; } = string.Empty;
+        public string MetricsEndpoint { get; set; } = string.Empty;
+        public string TelemetryCollectorEndpoint { get; set; } = string.Empty;
+    }
+}
---
+++ LinkAdmin.BFF/Infrastructure/Extensions/TelemetryServiceExtension.cs
@@ -0,0 +1,60 @@
+﻿using LantanaGroup.Link.LinkAdmin.BFF.Application.Models;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace LantanaGroup.Link.LinkAdmin.BFF.Infrastructure.Extensions
+{
+    public static class TelemetryServiceExtension
+    {
+        public static IServiceCollection AddOpenTelemetryService(this IServiceCollection services, TelemetryConfig telemetryConfig, IWebHostEnvironment env)
+        {
+
+            var otel = services.AddOpenTelemetry();
+
+            //configure OpenTelemetry resources with application name
+            otel.ConfigureResource(resource => resource
+                .AddService(
+                    serviceName: ServiceActivitySource.Instance.Name,
+                    serviceVersion: ServiceActivitySource.Instance.Version
+                ));
+
+            otel.WithTracing(tracerProviderBuilder =>
+                    tracerProviderBuilder
+                        .AddSource(ServiceActivitySource.Instance.Name)
+                        .AddAspNetCoreInstrumentation(options =>
+                        {
+                            options.Filter = (httpContext) => httpContext.Request.Path != "/health"; //do not capture traces for the health check endpoint                                                           
+                        })
+                        .AddOtlpExporter(opts => { opts.Endpoint = new Uri(telemetryConfig.TelemetryCollectorEndpoint); }));
+
+            otel.WithMetrics(metricsProviderBuilder =>
+                    metricsProviderBuilder
+                        .AddAspNetCoreInstrumentation()
+                        .AddProcessInstrumentation()
+                        .AddOtlpExporter(opts => { opts.Endpoint = new Uri(telemetryConfig.TelemetryCollectorEndpoint); }));
+
+            if (telemetryConfig.EnableRuntimeInstrumentation)
+            {
+                otel.WithMetrics(metricsProviderBuilder =>
+                    metricsProviderBuilder
+                        .AddRuntimeInstrumentation());
+            }
+
+            if (env.IsDevelopment())
+            {
+                otel.WithTracing(tracerProviderBuilder =>
+                    tracerProviderBuilder
+                     .AddConsoleExporter());
+
+                //metrics are very verbose, only enable console exporter if you really want to see metric details
+                //otel.WithMetrics(metricsProviderBuilder =>
+                //    metricsProviderBuilder
+                //        .AddConsoleExporter());                
+            }
+
+            return services;
+        }     
+        
+    }
+}
