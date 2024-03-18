4d7b7f1232ca6969723fcc1d5342a7b2b8386dc5
Adam Phillips <adam.phillips@lantanagroup.com>
2024-03-18 13:44:19 -0400

    Re-organize and setup middleware

--- LinkAdmin.BFF/Program.cs
+++ LinkAdmin.BFF/Program.cs
@@ -1,8 +1,10 @@
 using Azure.Identity;
+using HealthChecks.UI.Client;
 using LantanaGroup.Link.LinkAdmin.BFF.Application.Models;
 using LantanaGroup.Link.LinkAdmin.BFF.Infrastructure;
 using LantanaGroup.Link.LinkAdmin.BFF.Infrastructure.Extensions;
 using LantanaGroup.Link.LinkAdmin.BFF.Settings;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Serilog;
 using Serilog.Enrichers.Span;
@@ -13,126 +15,151 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//load external configuration source if specified
-var externalConfigurationSource = builder.Configuration.GetSection(LinkAdminConstants.AppSettingsSectionNames.ExternalConfigurationSource).Get<string>();
-if (!string.IsNullOrEmpty(externalConfigurationSource))
+RegisterServices(builder);
+var app = builder.Build();
+SetupMiddleware(app);
+
+app.Run();
+
+#region Register Services
+static void RegisterServices(WebApplicationBuilder builder)
 {
-    switch (externalConfigurationSource)
+    //load external configuration source if specified
+    var externalConfigurationSource = builder.Configuration.GetSection(LinkAdminConstants.AppSettingsSectionNames.ExternalConfigurationSource).Get<string>();
+    if (!string.IsNullOrEmpty(externalConfigurationSource))
     {
-        case ("AzureAppConfiguration"):
-            builder.Configuration.AddAzureAppConfiguration(options =>
-            {
-                options.Connect(builder.Configuration.GetConnectionString("AzureAppConfiguration"))
-                    // Load configuration values with no label
-                    .Select("Link:AdminBFF*", LabelFilter.Null)
-                    // Override with any configuration values specific to current hosting env
-                    .Select("Link:AdminBFF*", builder.Environment.EnvironmentName);
-
-                options.ConfigureKeyVault(kv =>
+        switch (externalConfigurationSource)
+        {
+            case ("AzureAppConfiguration"):
+                builder.Configuration.AddAzureAppConfiguration(options =>
                 {
-                    kv.SetCredential(new DefaultAzureCredential());
-                });
+                    options.Connect(builder.Configuration.GetConnectionString("AzureAppConfiguration"))
+                        // Load configuration values with no label
+                        .Select("Link:AdminBFF*", LabelFilter.Null)
+                        // Override with any configuration values specific to current hosting env
+                        .Select("Link:AdminBFF*", builder.Environment.EnvironmentName);
 
-            });
-            break;
+                    options.ConfigureKeyVault(kv =>
+                    {
+                        kv.SetCredential(new DefaultAzureCredential());
+                    });
+
+                });
+                break;
+        }
+    }
+
+    var serviceInformation = builder.Configuration.GetRequiredSection(LinkAdminConstants.AppSettingsSectionNames.ServiceInformation).Get<ServiceInformation>();
+    if (serviceInformation != null)
+    {
+        ServiceActivitySource.Initialize(serviceInformation);
+    }
+    else
+    {
+        throw new NullReferenceException("Service Information was null.");
+    }
+
+    //configure CORS
+    builder.Services.AddCorsService(builder.Environment);    
+
+    //Add problem details
+    builder.Services.AddProblemDetails(options => {
+        options.CustomizeProblemDetails = ctx =>
+        {
+            ctx.ProblemDetails.Detail = "An error occured in our API. Please use the trace id when requesting assistence.";
+            if (!ctx.ProblemDetails.Extensions.ContainsKey("traceId"))
+            {
+                string? traceId = Activity.Current?.Id ?? ctx.HttpContext.TraceIdentifier;
+                ctx.ProblemDetails.Extensions.Add(new KeyValuePair<string, object?>("traceId", traceId));
+            }
+
+            if (builder.Environment.IsDevelopment())
+            {
+                ctx.ProblemDetails.Extensions.Add("API", "Link Administration");
+            }
+            else
+            {
+                ctx.ProblemDetails.Extensions.Remove("exception");
+            }
+
+        };
+    });
+
+    //Add health checks
+    builder.Services.AddHealthChecks();
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+    });
+
+    // Logging using Serilog
+    builder.Logging.AddSerilog();
+    var loggerOptions = new ConfigurationReaderOptions { SectionName = LinkAdminConstants.AppSettingsSectionNames.Serilog };
+    Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(builder.Configuration, loggerOptions)
+                    .Filter.ByExcluding("RequestPath like '/health%'")
+                    .Filter.ByExcluding("RequestPath like '/swagger%'")
+                    .Enrich.WithExceptionDetails()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithSpan()
+                    .Enrich.With<ActivityEnricher>()
+                    .CreateLogger();
+
+    //Serilog.Debugging.SelfLog.Enable(Console.Error); 
+
+    // Add open telemetry
+    var telemetryConfig = builder.Configuration.GetSection(LinkAdminConstants.AppSettingsSectionNames.Telemetry).Get<TelemetryConfig>();
+    if (telemetryConfig != null)
+    {
+        builder.Services.AddOpenTelemetryService(telemetryConfig, builder.Environment);
     }
 }
 
-var serviceInformation = builder.Configuration.GetRequiredSection(LinkAdminConstants.AppSettingsSectionNames.ServiceInformation).Get<ServiceInformation>();
-if (serviceInformation != null)
-{
-    ServiceActivitySource.Initialize(serviceInformation);
-}
-else
-{
-    throw new NullReferenceException("Service Information was null.");
-}
+#endregion
 
-//configure CORS
-builder.Services.AddCorsService(builder.Environment);
 
-// Add open telemetry
-var telemetryConfig = builder.Configuration.GetSection(LinkAdminConstants.AppSettingsSectionNames.Telemetry).Get<TelemetryConfig>();
-if (telemetryConfig != null)
+#region Setup Middleware
+static void SetupMiddleware(WebApplication app)
 {
-    builder.Services.AddOpenTelemetryService(telemetryConfig, builder.Environment);
-}
-
-//Add problem details
-builder.Services.AddProblemDetails(options => {
-    options.CustomizeProblemDetails = ctx =>
+    if (app.Environment.IsDevelopment())
     {
-        ctx.ProblemDetails.Detail = "An error occured in our API. Please use the trace id when requesting assistence.";
-        if (!ctx.ProblemDetails.Extensions.ContainsKey("traceId"))
-        {
-            string? traceId = Activity.Current?.Id ?? ctx.HttpContext.TraceIdentifier;
-            ctx.ProblemDetails.Extensions.Add(new KeyValuePair<string, object?>("traceId", traceId));
-        }
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler();
+    }
 
-        if (builder.Environment.IsDevelopment())
-        {
-            ctx.ProblemDetails.Extensions.Add("API", "Link Administration");
-        }
-        else
-        {
-            ctx.ProblemDetails.Extensions.Remove("exception");
-        }
+    //configure swagger
+    if (app.Configuration.GetValue<bool>(LinkAdminConstants.AppSettingsSectionNames.EnableSwagger))
+    {
+        var serviceInformation = app.Configuration.GetSection(LinkAdminConstants.AppSettingsSectionNames.ServiceInformation).Get<ServiceInformation>();
+        app.UseSwagger();
+        app.UseSwaggerUI(opts => opts.SwaggerEndpoint("/swagger/v1/swagger.json", serviceInformation != null ? $"{serviceInformation.Name} - {serviceInformation.Version}" : "Link Admin API"));
+    }
 
-    };
-});
+    app.UseHttpsRedirection();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});
+    app.UseRouting();
+    app.UseCors("CorsPolicy");
+    app.UseAuthentication();
+    //app.UseMiddleware<UserScopeMiddleware>();
+    app.UseAuthorization();
 
-// Logging using Serilog
-builder.Logging.AddSerilog();
-var loggerOptions = new ConfigurationReaderOptions { SectionName = LinkAdminConstants.AppSettingsSectionNames.Serilog };
-Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Configuration, loggerOptions)
-                .Filter.ByExcluding("RequestPath like '/health%'")
-                .Filter.ByExcluding("RequestPath like '/swagger%'")
-                .Enrich.WithExceptionDetails()
-                .Enrich.FromLogContext()
-                .Enrich.WithSpan()
-                .Enrich.With<ActivityEnricher>()
-                .CreateLogger();
-
-//Serilog.Debugging.SelfLog.Enable(Console.Error);  
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //map health check middleware
+    app.MapHealthChecks("/health", new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 }
 
-app.UseHttpsRedirection();
+#endregion
 
 
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast")
-//.WithOpenApi();
-
-app.Run();
 
