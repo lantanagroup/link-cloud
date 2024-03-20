80191d8cc50fd4af8ab792376eeed4fe86c91b6e
MontaltoNick <nick.montalto@lantanagroup.com>
2024-03-20 14:26:56 -0500

    Merge

--- Shared/Application/Error/Interfaces/ITransientExceptionHandler.cs
+++ Shared/Application/Error/Interfaces/ITransientExceptionHandler.cs
@@ -1,12 +1,21 @@
 ﻿using Confluent.Kafka;
-using LantanaGroup.Link.Shared.Application.Interfaces;
 using LantanaGroup.Link.Shared.Application.Models.Kafka;
 
 namespace LantanaGroup.Link.Shared.Application.Error.Interfaces
 {
     public interface ITransientExceptionHandler<K, V>
     {
-        void HandleException(ConsumeResult<K, V> consumeResult, Exception ex);
+        /// <summary>
+        /// The Topic to use when publishing Retry Kafka events.
+        /// </summary>
+        public string Topic { get; set; }
+
+        /// <summary>
+        /// The name of the service that is consuming the ITransientExceptionHandler.
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        void HandleException(ConsumeResult<K, V> consumeResult, Exception ex, string facilityId);
         void ProduceAuditEvent(AuditEventMessage auditValue, Headers headers);
         void ProduceRetryScheduledEvent(K key, V value, Headers headers);
     }
