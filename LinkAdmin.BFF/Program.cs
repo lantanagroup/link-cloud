2c8c06dc4a9722c8e3b28a7d6b78cb173a6478b7
Adam Phillips <adam.phillips@lantanagroup.com>
2024-03-27 11:57:34 -0400

    Added authorization to swagger

--- LinkAdmin.BFF/Program.cs
+++ LinkAdmin.BFF/Program.cs
@@ -17,6 +17,8 @@ using Serilog.Enrichers.Span;
 using Serilog.Exceptions;
 using Serilog.Settings.Configuration;
 using System.Reflection;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -198,9 +200,81 @@ static void RegisterServices(WebApplicationBuilder builder)
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen(c =>
     {
+
+        if (builder.Configuration.GetValue<bool>("Authentication:Schemas:Jwt:Enabled"))
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = $"Authorization using JWT",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                }
+            });
+        }
+
+        c.AddSecurityDefinition("OAuth", new OpenApiSecurityScheme
+        {
+            Description = $"Authorization using OAuth",
+            Name = "OAuth",
+            Type = SecuritySchemeType.OAuth2,
+            Scheme = LinkAdminConstants.AuthenticationSchemes.Oauth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                AuthorizationCode = new OpenApiOAuthFlow
+                {                    
+                    AuthorizationUrl = new Uri(builder.Configuration.GetValue<string>("Authentication:Schemas:Oauth2:Endpoints:Authorization")!),
+                    TokenUrl = new Uri(builder.Configuration.GetValue<string>("Authentication:Schemas:Oauth2:Endpoints:Token")!),
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { "openid", "OpenId" },
+                        { "profile", "Profile" },
+                        { "email", "Email" }
+                    }
+                }
+            }
+
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Id = "OAuth",
+                        Type = ReferenceType.SecurityScheme
+                    },
+                    Scheme = LinkAdminConstants.AuthenticationSchemes.Oauth2,
+                    Name = "Oauth",
+                    In = ParameterLocation.Header
+
+                },
+                new List<string>()
+            }
+        });        
+
         var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
         var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
         c.IncludeXmlComments(xmlPath);
+
     });
 
     // Add logging redaction services
@@ -271,7 +345,7 @@ static void SetupMiddleware(WebApplication app)
     app.UseAuthorization(); 
 
     // Register endpoints
-    app.MapGet("/", (HttpContext ctx) => Results.Ok($"Welcome to {ServiceActivitySource.Instance.Name} version {ServiceActivitySource.Instance.Version}!"));
+    app.MapGet("/", (HttpContext ctx) => Results.Ok($"Welcome to {ServiceActivitySource.Instance.Name} version {ServiceActivitySource.Instance.Version}!")).AllowAnonymous();
 
     var apis = app.Services.GetServices<IApi>();
     foreach (var api in apis)
