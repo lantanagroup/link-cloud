6dc2b2390da4a75c63d7322d4eef2f5f725657bf
Sean McIlvenna <seanmcilvenna@users.noreply.github.com>
2024-04-01 16:05:44 -0700

    Changes to pipelines for validation (#81)
    * Updating validation pipelines to build java using maven
    
    * Specifying location of pom.xml file for validation
    
    * Updating path to be relative to root
    
    * Correcting case sensitivity
    
    * Settings file is relative to maven pom.xml
    
    * .m2/settings.xml file was being ignored, and is needed for devops
    
    * Installing java 17 in build agent
    
    * Using shared group of variables and specifying correct location of dockerfile
    
    * Removing old validation projects

--- Validation/Models/ServiceInformation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LantanaGroup.Link.Validation.Models
-{
-    public class ServiceInformation
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Version { get; set; } = string.Empty;
-    }
-}
