96b9921ca70c87f85b3cd913907bdf4470061ba7
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2024-04-01 14:33:07 -0400

    LNK-2159: Shared -Add- SQL - Persistence

---
+++ Shared/Application/Repositories/Interfaces/ISqlPersistenceRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LantanaGroup.Link.Shared.Application.Repositories.Interfaces
+{
+    public interface ISqlPersistenceRepository<T> where T : class
+    {
+        public Task<List<T>> GetAsync(CancellationToken cancellationToken);
+        public Task<T> GetAsyncById(Guid id, CancellationToken cancellationToken);
+        public Task<bool> CreateAsync(T entity, CancellationToken cancellationToken);
+        public Task<bool> UpdateAsync(T entity, CancellationToken cancellationToken);
+        public Task<bool> RemoveAsync(Guid id, CancellationToken cancellationToken);
+
+    }
+}
