2256d2f423036ee9871aceaf3fedb294ca102ef9
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2024-04-03 08:43:56 -0700

    Applying fixes to new java measure eval service to successfully connect to mongo database

--- Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/entities/PatientResource.java
+++ Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/entities/PatientResource.java
@@ -1,17 +1,12 @@
 package com.lantanagroup.link.measureeval.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import org.hl7.fhir.r4.model.ResourceType;
+import org.springframework.data.annotation.Id;
 
 import java.time.Instant;
-import java.util.Date;
 
-@Entity
 public class PatientResource {
     @Id
-    @GeneratedValue
     private String id;
 
     private String tenantId;
