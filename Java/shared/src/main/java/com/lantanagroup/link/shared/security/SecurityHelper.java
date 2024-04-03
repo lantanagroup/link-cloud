a9c385ecf60ddb3780591c8ec401427c8e6dab4e
Adam Phillips <adam.phillips@lantanagroup.com>
2024-04-03 09:15:00 -0400

    moved jwt secutiy filter chain logic into the shared security helper method.
    Added configuration necessary for measure evaluation to use the scurity filter chain.

--- Java/shared/src/main/java/com/lantanagroup/link/shared/security/SecurityHelper.java
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/security/SecurityHelper.java
@@ -1,16 +1,28 @@
 package com.lantanagroup.link.shared.security;
 
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityHelper {
     public static SecurityFilterChain build(HttpSecurity http) throws Exception {
-        // TODO: This needs to be updated to secure the application
         http
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .anyRequest()
-                                .anonymous());
+                .authorizeHttpRequests(authorizeRequests -> {
+                    //TODO: Add more specific authorization rules here
+                    // - create security folder in src/main/java/com/lantanagroup/link/validation
+                    // - create a new Authorization Manager classes for specific authorization rules in the security folder
+                    // - consider putting the Authorization Manager classes in the shared module
+                    // - Ex: authorizeRequests.requestMatchers("/endpoint").access(customAuthorizationManager());
+                    // - Ex: authorizeRequests.requestMatchers("/endpoint").hasRole("ROLE_USER");
+                    authorizeRequests.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(resourceServer -> {
+                    resourceServer.jwt(Customizer.withDefaults());
+                })
+                .sessionManagement(sessionManagement -> {
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
         return http.build();
     }
 }
