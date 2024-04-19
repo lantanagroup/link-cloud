785ec4e93d0a464348f28e7de7dbbb1b0ec8ecc8
Adam Phillips <adam.phillips@lantanagroup.com>
2024-04-19 08:43:49 -0400

    Fixes for shared TenantApiService

--- DotNet/Shared/Application/Services/TenantApiService.cs
+++ DotNet/Shared/Application/Services/TenantApiService.cs
@@ -1,5 +1,6 @@
 ﻿using LantanaGroup.Link.Shared.Application.Models.Configs;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Net;
 
 namespace LantanaGroup.Link.Shared.Application.Services;
@@ -8,9 +9,9 @@ public class TenantApiService : ITenantApiService
 {
     private readonly ILogger<TenantApiService> _logger;
     private readonly IHttpClientFactory _httpClientFactory;
-    private readonly ServiceRegistry _serviceRegistry;
+    private readonly IOptions<ServiceRegistry> _serviceRegistry;
 
-    public TenantApiService(ILogger<TenantApiService> logger, IHttpClientFactory httpClientFactory, ServiceRegistry serviceRegistry)
+    public TenantApiService(ILogger<TenantApiService> logger, IHttpClientFactory httpClientFactory, IOptions<ServiceRegistry> serviceRegistry)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
@@ -19,16 +20,22 @@ public class TenantApiService : ITenantApiService
 
     public async Task<bool> CheckFacilityExists(string facilityId, CancellationToken cancellationToken = default)
     {
-        if (_serviceRegistry.TenantService == null)
+        if (_serviceRegistry.Value.TenantService == null)
             throw new Exception("Tenant Service configuration is missing.");
 
-        if (!_serviceRegistry.TenantService.CheckIfTenantExists)
+        if (!_serviceRegistry.Value.TenantService.CheckIfTenantExists)
             return true;
 
+        var tenantServiceApiUrl = _serviceRegistry.Value.TenantService.TenantServiceApiUrl;
+
+        if (string.IsNullOrWhiteSpace(tenantServiceApiUrl))
+            throw new Exception("Tenant Service URL is missing.");
+
         var httpClient = _httpClientFactory.CreateClient();
-        var endpoint = $"{_serviceRegistry.TenantService.TenantServiceUrl.TrimEnd('/')}/{_serviceRegistry.TenantService.GetTenantRelativeEndpoint.TrimStart('/').TrimEnd('/')}/{facilityId.TrimStart('/').TrimEnd('/')}";
-        _logger.LogInformation("Tenant Base Endpoint: {0}", _serviceRegistry.TenantService.TenantServiceUrl);
-        _logger.LogInformation("Tenant Relative Endpoint: {0}", _serviceRegistry.TenantService.GetTenantRelativeEndpoint);
+
+        var endpoint = $"{tenantServiceApiUrl.TrimStart('/').TrimEnd('/')}/facility/{facilityId.TrimStart('/').TrimEnd('/')}";
+        _logger.LogInformation("Tenant Base Endpoint: {0}", tenantServiceApiUrl);
+        _logger.LogInformation("Tenant Relative Endpoint: {0}", _serviceRegistry.Value.TenantService.GetTenantRelativeEndpoint);
         _logger.LogInformation("Checking if facility ({1}) exists in Tenant Service. Endpoint: {2}", facilityId, endpoint);
         var response = await httpClient.GetAsync(endpoint, cancellationToken);
 
