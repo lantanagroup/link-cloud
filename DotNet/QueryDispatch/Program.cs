1c597cc5a895c279814f23181b6729ec0a42ca73
Adam Phillips <adam.phillips@lantanagroup.com>
2024-04-21 18:54:58 -0400

    Updated serilog, updated service name to match what is in azure app config, updated telemetry and configuration to use shared service extension

--- DotNet/QueryDispatch/Program.cs
+++ DotNet/QueryDispatch/Program.cs
@@ -1,5 +1,4 @@
 using Azure.Identity;
-using Confluent.Kafka.Extensions.OpenTelemetry;
 using HealthChecks.UI.Client;
 using LanatanGroup.Link.QueryDispatch.Jobs;
 using LantanaGroup.Link.QueryDispatch.Application.Factory;
@@ -11,13 +10,13 @@ using LantanaGroup.Link.QueryDispatch.Application.QueryDispatchConfiguration.Com
 using LantanaGroup.Link.QueryDispatch.Application.ScheduledReport.Commands;
 using LantanaGroup.Link.QueryDispatch.Application.ScheduledReport.Queries;
 using LantanaGroup.Link.QueryDispatch.Listeners;
-using LantanaGroup.Link.QueryDispatch.Persistence;
 using LantanaGroup.Link.QueryDispatch.Persistence.PatientDispatch;
 using LantanaGroup.Link.QueryDispatch.Persistence.QueryDispatchConfiguration;
 using LantanaGroup.Link.QueryDispatch.Persistence.ScheduledReport;
 using LantanaGroup.Link.QueryDispatch.Presentation.Services;
 using LantanaGroup.Link.Shared.Application.Error.Handlers;
 using LantanaGroup.Link.Shared.Application.Error.Interfaces;
+using LantanaGroup.Link.Shared.Application.Extensions;
 using LantanaGroup.Link.Shared.Application.Factories;
 using LantanaGroup.Link.Shared.Application.Interfaces;
 using LantanaGroup.Link.Shared.Application.Models.Configs;
@@ -25,19 +24,20 @@ using LantanaGroup.Link.Shared.Application.Models.Kafka;
 using LantanaGroup.Link.Shared.Application.Repositories.Implementations;
 using LantanaGroup.Link.Shared.Application.Services;
 using LantanaGroup.Link.Shared.Jobs;
+using LantanaGroup.Link.Shared.Settings;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 using Quartz;
 using Quartz.Impl;
 using Quartz.Spi;
+using QueryDispatch.Application.Interfaces;
 using QueryDispatch.Application.Models;
 using QueryDispatch.Application.Services;
 using QueryDispatch.Application.Settings;
 using QueryDispatch.Presentation.Services;
 using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
 using System.Reflection;
 using System.Text.Json.Serialization;
 
@@ -74,8 +74,7 @@ if (!string.IsNullOrEmpty(externalConfigurationSource))
 var serviceInformation = builder.Configuration.GetRequiredSection(QueryDispatchConstants.AppSettingsSectionNames.ServiceInformation).Get<ServiceInformation>();
 if (serviceInformation != null)
 {
-    ServiceActivitySource.Initialize(serviceInformation);
-    Counters.Initialize(serviceInformation);
+    ServiceActivitySource.Initialize(serviceInformation);    
 }
 else
 {
@@ -174,45 +173,25 @@ builder.Services.AddHealthChecks()
 builder.Logging.AddSerilog();
 Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Configuration)
+                .Filter.ByExcluding("RequestPath like '/health%'")
+                .Filter.ByExcluding("RequestPath like '/swagger%'")
+                .Enrich.WithExceptionDetails()
+                .Enrich.FromLogContext()
+                .Enrich.WithSpan()
+                .Enrich.With<ActivityEnricher>()
                 .CreateLogger();
 
-var telemetryConfig = builder.Configuration.GetRequiredSection(QueryDispatchConstants.AppSettingsSectionNames.Telemetry).Get<TelemetryConfig>();
-if (telemetryConfig != null)
+//Add telemetry if enabled
+if (builder.Configuration.GetValue<bool>($"{ConfigurationConstants.AppSettings.Telemetry}:EnableTelemetry"))
 {
-    var otel = builder.Services.AddOpenTelemetry();
-
-    //configure OpenTelemetry resources with application name
-    otel.ConfigureResource(resource => resource
-        .AddService(ServiceActivitySource.Instance.Name, ServiceActivitySource.Instance.Version));
-
-    otel.WithTracing(tracerProviderBuilder =>
-            tracerProviderBuilder
-                .AddSource(ServiceActivitySource.Instance.Name)
-                .AddAspNetCoreInstrumentation(options =>
-                {
-                    options.Filter = (httpContext) => httpContext.Request.Path != "/health"; //do not capture traces for the health check endpoint
-                })
-                .AddConfluentKafkaInstrumentation()
-                .AddOtlpExporter(opts => { opts.Endpoint = new Uri(telemetryConfig.TelemetryCollectorEndpoint); }));
-
-    otel.WithMetrics(metricsProviderBuilder =>
-            metricsProviderBuilder
-                .AddAspNetCoreInstrumentation()
-                .AddRuntimeInstrumentation()
-                .AddMeter(Counters.meter.Name)
-                .AddOtlpExporter(opts => { opts.Endpoint = new Uri(telemetryConfig.TelemetryCollectorEndpoint); }));
-
-    if (builder.Environment.IsDevelopment())
+    builder.Services.AddLinkTelemetry(builder.Configuration, options =>
     {
-        otel.WithTracing(tracerProviderBuilder =>
-            tracerProviderBuilder
-            .AddConsoleExporter());
+        options.Environment = builder.Environment;
+        options.ServiceName = QueryDispatchConstants.ServiceName;
+        options.ServiceVersion = serviceInformation.Version; //TODO: Get version from assembly?                
+    });
 
-        //metrics are very verbose, only enable console exporter if you really want to see metric details
-        //otel.WithMetrics(metricsProviderBuilder =>
-        //    metricsProviderBuilder
-        //        .AddConsoleExporter());                
-    }
+    builder.Services.AddSingleton<IQueryDispatchServiceMetrics, QueryDispatchServiceMetrics>();
 }
 
 var app = builder.Build();
