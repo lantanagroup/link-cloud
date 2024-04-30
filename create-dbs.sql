9595f940eab91520cd7931d44c097024bc662a02
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2024-04-30 11:40:03 -0700

    Aligning Serilog to use the same order of sinks across services Worked on normalization docker compose

--- create-dbs.sql
+++ create-dbs.sql
@@ -1,4 +1,5 @@
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'link-audit') CREATE DATABASE [link-audit];
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'link-census') CREATE DATABASE [link-census];
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'link-dataacquisition') CREATE DATABASE [link-dataacquisition];
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'link-normalization') CREATE DATABASE [link-normalization];
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'link-tenant') CREATE DATABASE [link-tenant];
