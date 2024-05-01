51b83f694fd36b9abd72d2c0da1dc3e38a72cded
MontaltoNick <nick.montalto@lantanagroup.com>
2024-05-01 16:34:55 -0500

    Undo name change

--- DotNet/Shared/Application/Models/Kafka/RetryEntity.cs
+++ DotNet/Shared/Application/Models/Kafka/RetryEntity.cs
@@ -1,13 +1,16 @@
-﻿using LantanaGroup.Link.Shared.Domain.Attributes;
+﻿using Confluent.Kafka;
+using LantanaGroup.Link.Shared.Application.Models.Configs;
+using LantanaGroup.Link.Shared.Domain.Attributes;
 using LantanaGroup.Link.Shared.Domain.Entities;
 using MongoDB.Bson.Serialization.Attributes;
+using StackExchange.Redis;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LantanaGroup.Link.Shared.Application.Models
 {
     [BsonCollection("retryEntity")]
     [BsonIgnoreExtraElements]
-    [Table("RetryEvent")]
+    [Table("kafkaRetryTbl")]
     public class RetryEntity : BaseEntity
     {
         public string ServiceName { get; set; }
