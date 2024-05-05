2d0f3cbc65c510eb3fd95d7bdca535fb2c919014
Adam Phillips <adam.phillips@lantanagroup.com>
2024-05-05 09:43:47 -0400

    implemented search respository

--- DotNet/Account/Application/Interfaces/Persistence/ISearchRepository.cs
+++ DotNet/Account/Application/Interfaces/Persistence/ISearchRepository.cs
@@ -6,6 +6,8 @@ namespace LantanaGroup.Link.Account.Application.Interfaces.Persistence
 {
     public interface ISearchRepository
     {
-        Task<(IEnumerable<LinkUser>, IPaginationMetadata)> SearchAsync(string? searchText, string? filterFacilityBy, string? filterRoleBy, string? filterClaimBy, bool isActive, bool isDeleted, string? sortBy, SortOrder? sortOrder, int pageSize, int pageNumber, CancellationToken cancellationToken = default);
+        Task<(IEnumerable<LinkUser>, IPaginationMetadata)> SearchAsync(string? searchText, string? filterFacilityBy, string? filterRoleBy, string? filterClaimBy, bool includeDeactivatedUsers, bool includeDeletedUsers, string? sortBy, SortOrder? sortOrder, int pageSize, int pageNumber, CancellationToken cancellationToken = default);
+
+        Task<(IEnumerable<LinkUser>, IPaginationMetadata)> TenantSearchAsync(string filterFacilityBy, string? searchText, string? filterRoleBy, string? filterClaimBy, string? sortBy, SortOrder? sortOrder, int pageSize, int pageNumber, CancellationToken cancellationToken = default);
     }
 }
