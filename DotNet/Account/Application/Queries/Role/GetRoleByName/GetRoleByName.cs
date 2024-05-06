fe5a45dbaaeb66a725972a06069483bcc5d8f9ca
Adam Phillips <adam.phillips@lantanagroup.com>
2024-05-05 20:19:30 -0400

    Added get role by name query

--- DotNet/Account/Application/Queries/Role/GetRoleByName/GetRoleByName.cs
+++ DotNet/Account/Application/Queries/Role/GetRoleByName/GetRoleByName.cs
@@ -6,29 +6,29 @@ using LantanaGroup.Link.Account.Infrastructure.Logging;
 using OpenTelemetry.Trace;
 using System.Diagnostics;
 
-namespace LantanaGroup.Link.Account.Application.Queries.Role.GetRole
+namespace LantanaGroup.Link.Account.Application.Queries.Role.GetRoleByName
 {
-    public class GetRole : IGetRole
+    public class GetRoleByName : IGetRoleByName
     {
-        private readonly ILogger<GetRole> _logger;
+        private readonly ILogger<GetRoleByName> _logger;
         private readonly IRoleRepository _roleRepository;
         private readonly ILinkRoleModelFactory _linkRoleModelFactory;
 
-        public GetRole(ILogger<GetRole> logger, IRoleRepository roleRepository, ILinkRoleModelFactory linkRoleModelFactory)
+        public GetRoleByName(ILogger<GetRoleByName> logger, IRoleRepository roleRepository, ILinkRoleModelFactory linkRoleModelFactory)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _roleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
             _linkRoleModelFactory = linkRoleModelFactory ?? throw new ArgumentNullException(nameof(linkRoleModelFactory));
         }
 
-        public async Task<LinkRoleModel> Execute(string roleId, CancellationToken cancellationToken = default)
+        public async Task<LinkRoleModel> Execute(string name, CancellationToken cancellationToken = default)
         {
-            using Activity? activity = ServiceActivitySource.Instance.StartActivity("GetRole:Execute");
+            using Activity? activity = ServiceActivitySource.Instance.StartActivity("GetRoleByName.Execute");
 
             try
             {
-                var role = await _roleRepository.GetRoleAsync(roleId, cancellationToken: cancellationToken) ?? 
-                    throw new ApplicationException($"Role with id {roleId} not found");
+                var role = await _roleRepository.GetRoleByNameAsync(name, cancellationToken: cancellationToken) ?? 
+                    throw new ApplicationException($"Role with name {name} not found");
 
                 return _linkRoleModelFactory.Create(role);
             }
@@ -36,7 +36,7 @@ namespace LantanaGroup.Link.Account.Application.Queries.Role.GetRole
             {
                 Activity.Current?.SetStatus(ActivityStatusCode.Error);
                 Activity.Current?.RecordException(ex);
-                _logger.LogFindRoleException(roleId, ex.Message);
+                _logger.LogFindRoleException(name, ex.Message);
                 throw;
             }
         }
