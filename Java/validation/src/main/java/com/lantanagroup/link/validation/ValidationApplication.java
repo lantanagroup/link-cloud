06613da4096b02248af89cf3a0c0b638e77a831f
Steven Williams <steven.williams@lantanagroup.com>
2024-05-08 07:48:51 -0400

    Fix `validation` initialization
      - Replace Mongo starter dependency
      - Use Spring Boot plugin (in module POM only)
      - Enable JPA repositories

--- Java/validation/src/main/java/com/lantanagroup/link/validation/ValidationApplication.java
+++ Java/validation/src/main/java/com/lantanagroup/link/validation/ValidationApplication.java
@@ -3,10 +3,10 @@ package com.lantanagroup.link.validation;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@PropertySource("classpath:application.yml")
+@EnableJpaRepositories
 public class ValidationApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(ValidationApplication.class);
