929b8c661a3fa22cfd7d8372fe7b1b82a5aaeac7
Adam Phillips <adam.phillips@lantanagroup.com>
2024-05-15 11:41:45 -0400

    Added seed data script

---
+++ DotNet/Account/Persistence/Scripts/AccountSeedData.sql
@@ -0,0 +1,17 @@
+--Set variables
+DECLARE @LinkPermissionClaimType varchar(128);
+DECLARE @IsLinkAdminClaim varchar(128)
+DECLARE @LinkUserRoleId uniqueidentifier
+DECLARE @UserId uniqueidentifier
+
+SET @LinkPermissionClaimType = 'permissions'
+SET @IsLinkAdminClaim = 'IsLinkAdmin'
+SET @LinkUserRoleId = NEWID()
+
+-- Add LinkUser role
+INSERT INTO Roles(Id, Name, Description, CreatedOn) VALUES(@LinkUserRoleId, 'LinkUser', 'A user of the Link application.', GETDATE())
+INSERT INTO RoleClaims(RoleId, ClaimType, ClaimValue) VALUES(@LinkUserRoleId, @LinkPermissionClaimType, @IsLinkAdminClaim)
+
+-- Add Seed Users
+--SET @UserId = NewID()
+--INSERT INTO Users(Id, Email, FirstName, LastName, CreatedOn, IsActive, IsDeleted) VALUES(@UserId, '<insertemail>', '<insertfirstname>', '<insertlastname>', GETDATE(), 1, 0)
