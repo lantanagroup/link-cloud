076ccb00ef60452836d4b1968097397b7ad808e2
Adam Phillips <adam.phillips@lantanagroup.com>
2024-05-26 09:54:03 -0400

    Added static link fhir serialization options and added some logic improvement to the fhir resource converter

--- DotNet/Shared/Application/Converters/FhirResourceConverter.cs
+++ DotNet/Shared/Application/Converters/FhirResourceConverter.cs
@@ -1,8 +1,7 @@
-﻿using Hl7.Fhir.ElementModel;
-using Hl7.Fhir.Serialization;
+﻿using Hl7.Fhir.Serialization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Hl7.Fhir.Model;
+using LantanaGroup.Link.Shared.Application.SerDes;
 
 namespace LantanaGroup.Link.Shared.Application.Converters
 {
@@ -10,23 +9,20 @@ namespace LantanaGroup.Link.Shared.Application.Converters
     {
         public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
+            var converterOptions = LinkFhirSerializerOptions.ForFhirWithoutValidation();            
+
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                return JsonSerializer.Deserialize<T>(ref reader, converterOptions);
+            }         
+       
+            // This is a string value, so we need to parse it as JSON
             using var jsonDocument = JsonDocument.ParseValue(ref reader);
             var jsonText = jsonDocument.RootElement.ToString().TrimStart('"').TrimEnd('"');
 
-            var converterOptions = new JsonSerializerOptions();
-            converterOptions.ForFhir(ModelInfo.ModelInspector, new FhirJsonPocoDeserializerSettings()
-            {
-                DisableBase64Decoding = false,
-                Validator = null,
-
-            });
-
-            converterOptions.AllowTrailingCommas = true;
-            converterOptions.PropertyNameCaseInsensitive = true;
-
             var resource = JsonSerializer.Deserialize<T>(jsonText, converterOptions);
 
-            return resource;
+            return resource;            
         }
 
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
