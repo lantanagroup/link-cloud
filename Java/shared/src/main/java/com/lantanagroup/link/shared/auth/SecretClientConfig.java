8f77aa18bcb179189ae1fb0bd8b22cd9c82a3a91
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2024-05-28 12:11:43 -0400

    LNK-2455: Enable Authentication/Authorization for Measure Eval service

--- Java/shared/src/main/java/com/lantanagroup/link/shared/auth/SecretClientConfig.java
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/auth/SecretClientConfig.java
@@ -3,15 +3,18 @@ package com.lantanagroup.link.shared.auth;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecretClientConfig {
 
+  @Value("${secretmanagement.manageruri}")
+  private String vaultUrl;
+
   @Bean
   public SecretClient createSecretClient() {
-    String vaultUrl = "https://link-vault.vault.azure.net/";
     return new SecretClientBuilder()
             .vaultUrl(vaultUrl)
             .credential(new DefaultAzureCredentialBuilder().build())
