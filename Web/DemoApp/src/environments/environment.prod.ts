6a73957859732eedb7dcf986530decf784a70b57
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2024-06-04 10:54:02 -0400

    LNK-2241 - reverse to the local settings

--- Web/DemoApp/src/environments/environment.prod.ts
+++ Web/DemoApp/src/environments/environment.prod.ts
@@ -1,4 +1,2 @@
 export const environment = {
-  production: true,
-  loginUrl: window.location.origin + '/',
 };
--- Web/DemoApp/src/environments/environment.ts
+++ Web/DemoApp/src/environments/environment.ts
@@ -3,8 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  loginUrl: ''
 };
 /*
  * For easier debugging in development mode, you can import the following file
