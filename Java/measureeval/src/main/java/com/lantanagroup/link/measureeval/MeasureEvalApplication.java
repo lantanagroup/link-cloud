00f3b9639ce143c705a5a0cfae78347655369480
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2024-07-10 09:31:10 -0400

    LNK-2552: Java-services-don't-prioritize-app-config

--- Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/MeasureEvalApplication.java
+++ Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/MeasureEvalApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@ComponentScan(basePackages = {"com.lantanagroup.link.measureeval", "com.lantanagroup.link.shared.logging"} )
 @ConfigurationPropertiesScan("com.lantanagroup.link.shared.config")
 public class MeasureEvalApplication {
     public static void main(String[] args) {
