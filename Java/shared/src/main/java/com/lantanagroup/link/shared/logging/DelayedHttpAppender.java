fe35c3c910ac41b3ebd23ad698e465623312fed6
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2024-07-10 12:18:57 -0400

    LNK-2552: Java-services-don't-prioritize-app-config

--- Java/shared/src/main/java/com/lantanagroup/link/shared/logging/DelayedHttpAppender.java
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/logging/DelayedHttpAppender.java
@@ -8,6 +8,20 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/*
+  * This class is a custom logback appender that extends Loki4jAppender. It is used to delay the logging of messages until the application is ready.
+  * This is useful when the application is logging messages before the Loki4jAppender is ready to accept them.
+  * The class uses a ConcurrentLinkedQueue to store messages until the application is ready.
+  * When the application is ready, the messages are processed and logged.
+  * The class also implements ApplicationListener to listen for the ApplicationReadyEvent.
+  * When the application is ready, the applicationReady flag is set to true and the processQueue method is called to log the messages in the queue.
+  * The append method is overridden to add messages to the queue if the application is not ready.
+  * If the application is ready, the processQueue method is called to log the messages in the queue.
+  * If the application is ready, the super.append method is called to log the message.
+  * The onApplicationEvent method is overridden to set the applicationReady flag to true when the application is ready.
+  * The loki appender is added to the logback configuration file to log messages to Loki.
+  * The loki url is already properly set from the app config by the time the application is ready. There is no attempt to send messages to Loki using the default url set in application.yml, before the application is ready and the url is properly set.
+ */
 @Component
 public class DelayedHttpAppender extends Loki4jAppender implements ApplicationListener<ApplicationReadyEvent> {
 
