b60b0c4364bbde71c081e47c9def3baefa8662a5
MontaltoNick <nick.montalto@lantanagroup.com>
2024-07-23 17:00:16 -0500

    Initial

--- DotNet/DataAcquisition/Application/Services/PatientDataService.cs
+++ DotNet/DataAcquisition/Application/Services/PatientDataService.cs
@@ -118,7 +118,7 @@ public class PatientDataService : IPatientDataService
                             PatientId = patientId,
                             QueryType = dataAcqRequested.QueryType
                         }
-                    });
+                    }, cancellationToken);
             }
 
             var queryPlan = queryPlans.FirstOrDefault(x => x.ReportType == scheduledReport.ReportType);
