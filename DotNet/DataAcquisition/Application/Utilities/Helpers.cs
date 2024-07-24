28b81fc44b4c56a4f3c15e4cff50086f0c5fc22d
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2024-07-24 09:40:36 -0500

    LNK-2775 Resolve Null Error in Resource Pulls

---
+++ DotNet/DataAcquisition/Application/Utilities/Helpers.cs
@@ -0,0 +1,10 @@
+﻿namespace LantanaGroup.Link.DataAcquisition.Application.Utilities;
+
+public static class Helpers
+{
+    public static string SplitReference(this string reference)
+    {
+        var splitReference = reference.Split("/");
+        return splitReference[splitReference.Length - 1];
+    }
+}
