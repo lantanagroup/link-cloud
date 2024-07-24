85ba63da2719fd481e7c5735417f8ce3d11b7528
Adam Phillips <adam.phillips@lantanagroup.com>
2024-07-24 13:33:54 -0400

    migrate to xunit

---
+++ DotNet/AuditTests/AuditEventProcessorTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace AuditTests
+{
+    public class AuditEventProcessorTests
+    {
+        [Fact]
+        public void AuditEventProcessor_ShouldNotAcceptNullMessageResults()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AuditEventProcessor_ShouldNotAcceptNullMessageValues()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AuditEventProcessor_CanExtractCorrelationIdIfPresent()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AuditEventProcessor_CanCreateAnAuditEvent()
+        {
+            Assert.True(true);
+        }
+    }
+}
