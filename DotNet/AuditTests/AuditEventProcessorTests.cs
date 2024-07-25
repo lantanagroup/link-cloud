f84dd43ee829fcd577919dc3622787d41c49606b
Adam Phillips <adam.phillips@lantanagroup.com>
2024-07-24 20:59:50 -0400

    Created audit event processor unit tests

--- DotNet/AuditTests/AuditEventProcessorTests.cs
+++ DotNet/AuditTests/AuditEventProcessorTests.cs
@@ -1,31 +1,78 @@
-﻿using Xunit;
+﻿using Confluent.Kafka;
+using LantanaGroup.Link.Audit.Application.Models;
+using LantanaGroup.Link.Audit.Application.Services;
+using LantanaGroup.Link.Audit.Domain.Entities;
+using LantanaGroup.Link.Audit.Domain.Managers;
+using LantanaGroup.Link.Shared.Application.Error.Exceptions;
+using LantanaGroup.Link.Shared.Application.Models;
+using LantanaGroup.Link.Shared.Application.Models.Kafka;
+using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace AuditTests
 {
     public class AuditEventProcessorTests
     {
+        private readonly Mock<ILogger<AuditEventProcessor>> _logger = new();
+        private readonly Mock<IAuditManager> _auditManagerMock = new();        
+
         [Fact]
-        public void AuditEventProcessor_ShouldNotAcceptNullMessageResults()
-        {
-            Assert.True(true);
+        public async Task AuditEventProcessor_ShouldNotAcceptNullMessageResults()
+        {                        
+            AuditEventProcessor proccessor = new(_logger.Object, _auditManagerMock.Object);
+            ConsumeResult<string, AuditEventMessage>? result = null;            
+
+            await Assert.ThrowsAsync<DeadLetterException>(() => proccessor.ProcessAuditEvent(result, It.IsAny<CancellationToken>()));
         }
 
         [Fact]
-        public void AuditEventProcessor_ShouldNotAcceptNullMessageValues()
+        public async Task AuditEventProcessor_ShouldNotAcceptNullMessageValues()
         {
-            Assert.True(true);
-        }
+            AuditEventProcessor proccessor = new(_logger.Object, _auditManagerMock.Object);
+            ConsumeResult<string, AuditEventMessage>? result = new()
+            {
+                Message = new Message<string, AuditEventMessage>() 
+                { 
+                    Value = null,  
+                    Headers = new Headers(),
+                    Key = "TestKey"
+                }
+            };
+
+            await Assert.ThrowsAsync<DeadLetterException>(() => proccessor.ProcessAuditEvent(result, It.IsAny<CancellationToken>()));
+
+        }        
 
         [Fact]
-        public void AuditEventProcessor_CanExtractCorrelationIdIfPresent()
+        public async Task AuditEventProcessor_CanCreateAnAuditEvent()
         {
-            Assert.True(true);
-        }
+            //set up message
+            AuditEventMessage message = new()
+            {
+                EventDate = DateTime.UtcNow,
+                ServiceName = "TestService",
+                FacilityId = "TestFacility",
+                Action = AuditEventType.Create,
+                Resource = "TestResource",
+                CorrelationId = Guid.NewGuid().ToString()
+            };
 
-        [Fact]
-        public void AuditEventProcessor_CanCreateAnAuditEvent()
-        {
-            Assert.True(true);
+            //create consume result
+            ConsumeResult<string, AuditEventMessage>? result = new()
+            {
+                Message = new Message<string, AuditEventMessage>()
+                {
+                    Value = message,
+                    Headers = new Headers(),
+                    Key = "TestKey"
+                }
+            };
+
+            _auditManagerMock.Setup(x => x.CreateAuditLog(It.IsAny<AuditModel>(), default)).ReturnsAsync(new AuditLog());
+            AuditEventProcessor proccessor = new(_logger.Object, _auditManagerMock.Object);
+            var outcome = await proccessor.ProcessAuditEvent(result, It.IsAny<CancellationToken>());            
+
+            Assert.True(outcome);
         }
     }
 }
