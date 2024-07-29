ef4558e2045b0307af0b91251b9b8cb7d0ff2b82
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2024-07-29 11:43:13 -0500

    Update Program.cs

--- DotNet/DataAcquisition/Program.cs
+++ DotNet/DataAcquisition/Program.cs
@@ -230,12 +230,7 @@ static void RegisterServices(WebApplicationBuilder builder)
     builder.Services.AddTransient<IRetryEntityFactory, RetryEntityFactory>();
     builder.Services.AddTransient<ISchedulerFactory, StdSchedulerFactory>();
     builder.Services.AddTransient<RetryJob>();
-    builder.Services.AddScoped<IJobFactory, JobFactory>();
-
-    builder.Services.Configure<HostOptions>(hostOptions =>
-    {
-        hostOptions.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
-    });
+    builder.Services.AddScoped<IJobFactory, JobFactory>(); 
 
     //Add Hosted Services
     if (!consumerSettings?.DisableConsumer ?? true)
