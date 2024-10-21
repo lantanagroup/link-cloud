a1ffcd159eff12a3a58618bb548f23ba4c335d8e
MontaltoNick <nick.montalto@lantanagroup.com>
2024-10-21 10:52:26 -0500

    DataAcquisition Mass Assignment

--- DotNet/DataAcquisition.Domain/Entities/QueryPlan.cs
+++ DotNet/DataAcquisition.Domain/Entities/QueryPlan.cs
@@ -1,18 +1,27 @@
 ﻿using LantanaGroup.Link.DataAcquisition.Domain.Interfaces;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 using LantanaGroup.Link.Shared.Domain.Entities;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Entities;
 
+[DataContract]
 [Table("queryPlan")]
 public class QueryPlan : BaseEntityExtended
 {
+    [DataMember]
     public string PlanName { get; set; }
+    [DataMember]
     public string ReportType { get; set; }
+    [DataMember]
     public string FacilityId { get; set; }
+    [DataMember]
     public string EHRDescription { get; set; }
+    [DataMember]
     public string LookBack { get; set; }
+    [DataMember]
     public Dictionary<string, IQueryConfig> InitialQueries { get; set; }
+    [DataMember]
     public Dictionary<string, IQueryConfig> SupplementalQueries { get; set; }
 
     public QueryPlan() : base()
