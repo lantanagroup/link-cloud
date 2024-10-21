342163bcdbeb9f2f971f1a77397b4f5bcb6758e1
MontaltoNick <nick.montalto@lantanagroup.com>
2024-10-21 13:22:00 -0500

    RC branch mass assignment edits

--- DotNet/DataAcquisition.Domain/Entities/FhirQueryConfiguration.cs
+++ DotNet/DataAcquisition.Domain/Entities/FhirQueryConfiguration.cs
@@ -1,25 +1,30 @@
 ﻿using LantanaGroup.Link.DataAcquisition.Domain.Models;
-using LantanaGroup.Link.Shared.Domain.Attributes;
-using System.ComponentModel.DataAnnotations.Schema;
 using LantanaGroup.Link.Shared.Domain.Entities;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Entities;
 
+[DataContract]
 [Table("fhirQueryConfiguration")]
 public class FhirQueryConfiguration : BaseEntityExtended
 {
+    [DataMember]
     [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public string FacilityId { get; set; }
 
+    [DataMember]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string FhirServerBaseUrl { get; set; }
 
+    [DataMember]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [BsonIgnoreIfNull]
     public AuthenticationConfiguration? Authentication { get; set; }
 
+    [DataMember]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? QueryPlanIds { get; set; }
 }
