dda00954ab2cf605893c17687894c3ffc13ed196
MontaltoNick <nick.montalto@lantanagroup.com>
2025-02-06 11:46:16 -0600

    Remove EnsureCreated

--- DotNet/Tenant/Program.cs
+++ DotNet/Tenant/Program.cs
@@ -272,13 +272,6 @@ namespace Tenant
                 ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
 
-            // Ensure database created
-            using (var scope = app.Services.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<FacilityDbContext>();
-                context.Database.EnsureCreated();
-
-            }
             // Configure the HTTP request pipeline.
             //app.MapGrpcService<TenantService>();
             //app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
