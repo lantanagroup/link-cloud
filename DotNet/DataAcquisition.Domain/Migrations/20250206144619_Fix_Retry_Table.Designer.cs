10d75b132787b70ef26ebb75b4e0969173db9e1b
MontaltoNick <nick.montalto@lantanagroup.com>
2025-02-06 08:54:20 -0600

    Dev Branch Fix

--- DotNet/DataAcquisition.Domain/Migrations/20250206144619_Fix_Retry_Table.Designer.cs
+++ DotNet/DataAcquisition.Domain/Migrations/20250206144619_Fix_Retry_Table.Designer.cs
@@ -4,6 +4,7 @@ using LantanaGroup.Link.DataAcquisition.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAcquisition.Domain.Migrations
 {
     [DbContext(typeof(DataAcquisitionDbContext))]
-    partial class DataAcquisitionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250206144619_Fix_Retry_Table")]
+    partial class Fix_Retry_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
