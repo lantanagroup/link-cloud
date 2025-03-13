6d2683a1239c94840bf2eefec21e48ae05059d99
Adam Phillips <adam.phillips@lantanagroup.com>
2025-03-13 16:40:14 -0400

    added link monitor section to the admin UI

---
+++ Web/Admin.UI/src/app/components/monitor/link-health-check/link-health-check.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-health-check',
+  imports: [],
+  templateUrl: './link-health-check.component.html',
+  styleUrl: './link-health-check.component.css'
+})
+export class LinkHealthCheckComponent {
+
+}
