9860e0528320ddadba4325fa32e9a31cb1e48826
Adam Phillips <adam.phillips@lantanagroup.com>
2025-03-14 14:04:21 -0400

    added table for health status

--- Web/Admin.UI/src/app/components/monitor/link-health-check/link-health-check.component.ts
+++ Web/Admin.UI/src/app/components/monitor/link-health-check/link-health-check.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MonitorService } from '../monitor.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { ILinkServiceHealthSummary } from './link-service-health-summary.interface';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-link-health-check',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatTableModule    
+  ],
   templateUrl: './link-health-check.component.html',
-  styleUrl: './link-health-check.component.css'
+  styleUrl: './link-health-check.component.scss'
 })
-export class LinkHealthCheckComponent {
+export class LinkHealthCheckComponent implements OnInit {
+  dataSource!: MatTableDataSource<ILinkServiceHealthSummary>;
+  healthSummary: ILinkServiceHealthSummary[] = [];
+  displayedColumns: string[] = ['ServiceName', 'ServiceHealthStatus', 'KafkaConnection', 'DatabaseConnection', 'CacheConnection'];
+
+  initializeSummaries = true;
+
+  constructor(private monitorService: MonitorService) { }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<ILinkServiceHealthSummary>();
+    this.getLinkHealthSummary();    
+  }
+
+  getLinkHealthSummary(): void {
+    this.monitorService.getLinkHealthCheck().subscribe({
+      next: (response: ILinkServiceHealthSummary[]) => {
+        this.healthSummary = response;
+        this.dataSource.data = this.healthSummary;
+        this.initializeSummaries = false;
+      },
+      error: (error) => {
+        console.error('Error fetching link health summary:', error);
+      } 
+    })
+  }
+
+  onRefresh(): void {
+    this.getLinkHealthSummary();
+  }
 
 }
