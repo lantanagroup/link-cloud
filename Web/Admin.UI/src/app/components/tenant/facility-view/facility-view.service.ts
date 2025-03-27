49b4ca8ae202afdb2266df7930b7eba86d410024
Adam Phillips <adam.phillips@lantanagroup.com>
2025-03-27 11:39:01 -0400

    Fixed error handling in facility view service.

--- Web/Admin.UI/src/app/components/tenant/facility-view/facility-view.service.ts
+++ Web/Admin.UI/src/app/components/tenant/facility-view/facility-view.service.ts
@@ -108,12 +108,12 @@ export class FacilityViewService {
             .pipe(
                 map((response: string[]) => {
                     return response;
-                },
+                }),
                 catchError((error: HttpErrorResponse) => {
                     this.errorHandler.handleError(error);
                     return [];
                 })
-            ));
+            );
     } 
 
     getReportValidationStatuses(): Observable<string[]> {
@@ -121,11 +121,11 @@ export class FacilityViewService {
             .pipe(
                 map((response: string[]) => {
                     return response;
-                },
+                }),
                 catchError((error: HttpErrorResponse) => {
                     this.errorHandler.handleError(error);
                     return [];
                 })
-            ));
+            );
     } 
 }
