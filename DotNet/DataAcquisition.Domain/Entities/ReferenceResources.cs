7a97a4a4c5a0803ef2acd329f264a19786685afb
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-04-10 13:57:23 -0500

    Lnk 3446 create acquisition priority enumeration (#780)
    * Create AcquisitionPriority.cs
    
    * Update ReferenceResources.cs

--- DotNet/DataAcquisition.Domain/Entities/ReferenceResources.cs
+++ DotNet/DataAcquisition.Domain/Entities/ReferenceResources.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using DataAcquisition.Domain.Models.Enums;
 using LantanaGroup.Link.Shared.Domain.Entities;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Entities;
 
-[Table("referenceResources")]
+[Table("ReferenceResources")]
 public class ReferenceResources : BaseEntityExtended
 {
     public string FacilityId { get; set; }
     public string ResourceId { get; set; }
     public string ResourceType { get; set; }
     public string ReferenceResource { get; set; }
+    
+    public QueryPhase QueryPhase { get; set; }
 }
