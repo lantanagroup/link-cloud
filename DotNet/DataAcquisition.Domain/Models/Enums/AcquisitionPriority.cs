c2dc281b0070cb2974b544dc6461b75aed0fc594
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-04-10 13:38:01 -0500

    LNK-3446: AcquisitionPriority (#779)
    Create AcquisitionPriority.cs

---
+++ DotNet/DataAcquisition.Domain/Models/Enums/AcquisitionPriority.cs
@@ -0,0 +1,17 @@
+﻿using LantanaGroup.Link.Shared.Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcquisition.Domain.Models.Enums;
+public enum AcquisitionPriority
+{
+    [StringValue("Normal")]
+    Normal,
+    [StringValue("High")]
+    High,
+    [StringValue("Critical")]
+    Critical
+}
