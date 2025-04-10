376dbdd2ae8cf3c48f40cbad0fe8d56bd646c9d7
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-04-10 14:02:37 -0500

    Lnk 3446 create acquisition priority enumeration (#781)
    * Create AcquisitionPriority.cs
    
    * Update ReferenceResources.cs
    
    * Create ScheduledReport.cs

---
+++ DotNet/DataAcquisition.Domain/Models/ScheduledReport.cs
@@ -0,0 +1,15 @@
+﻿using LantanaGroup.Link.Shared.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcquisition.Domain.Models;
+public class ScheduledReport
+{
+    public string[] ReportTypes { get; set; }
+    public Frequency Frequency { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+}
