1ee27f5db47a887d837271c4e5a57a856e109673
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-04-10 12:25:28 -0500

    Create RequestStatus.cs (#775)

---
+++ DotNet/DataAcquisition.Domain/Models/Enums/RequestStatus.cs
@@ -0,0 +1,19 @@
+﻿using LantanaGroup.Link.Shared.Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcquisition.Domain.Models.Enums;
+public enum RequestStatus
+{
+    [StringValue("Pending")]
+    Pending,
+    [StringValue("Processing")]
+    Processing,
+    [StringValue("Completed")]
+    Completed,
+    [StringValue("Failed")]
+    Failed
+}
