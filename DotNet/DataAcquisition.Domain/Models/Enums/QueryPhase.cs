c0c332b85a4c5f0f2b2b4348f2186ec93a4c6594
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-04-10 12:29:56 -0500

    Create QueryPhase.cs (#776)

---
+++ DotNet/DataAcquisition.Domain/Models/Enums/QueryPhase.cs
@@ -0,0 +1,21 @@
+﻿using LantanaGroup.Link.Shared.Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcquisition.Domain.Models.Enums;
+public enum QueryPhase
+{
+    [StringValue("Initial")]
+    Initial,
+    [StringValue("Supplemental")]
+    Supplemental,
+    [StringValue("Referential")]
+    Referential,
+    [StringValue("Polling")]
+    Polling,
+    [StringValue("Monitoring")]
+    Monitoring
+}
