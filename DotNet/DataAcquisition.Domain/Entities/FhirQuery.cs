64c04dcbe5ef4773ad600a45de435370eebbe7ff
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-04-10 13:33:01 -0500

    LNK-3445: Create FhirQuery Entity (#778)
    LNK-3445

--- DotNet/DataAcquisition.Domain/Entities/FhirQuery.cs
+++ DotNet/DataAcquisition.Domain/Entities/FhirQuery.cs
@@ -1,15 +1,18 @@
-﻿using LantanaGroup.Link.Shared.Domain.Entities;
+﻿using DataAcquisition.Domain.Entities;
+using DataAcquisition.Domain.Models.Enums;
+using Hl7.Fhir.Model;
+using LantanaGroup.Link.Shared.Domain.Entities;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Entities;
 
-[Table("fhirQuery")]
+[Table("FhirQuery")]
 public class FhirQuery : BaseEntityExtended
 {
     public string FacilityId { get; set; }
-    public string? CorrelationId { get; set; }
-    public string? PatientId { get; set; }
-    public string ResourceType { get; set; }
-    public string? SearchParams { get; set; }
-    public string? RequestBody { get; set; }
+    public FhirQueryType QueryType { get; set; }
+    public List<ResourceType> ResourceTypes { get; set; }
+    public List<string> QueryParameters { get; set; } = new List<string>();
+    public List<ResourceReferenceTypeEntity> ResourceReferenceType { get; set; }
+    public int? Paged { get; set; }
 }
---
+++ DotNet/DataAcquisition.Domain/Entities/ResourceReferenceTypeEntity.cs
@@ -0,0 +1,18 @@
+﻿using DataAcquisition.Domain.Models.Enums;
+using LantanaGroup.Link.Shared.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcquisition.Domain.Entities;
+
+[Table("ResourceReferenceType")]
+public class ResourceReferenceTypeEntity : BaseEntityExtended
+{
+    public string FacilityId { get; set; }
+    public QueryPhase QueryPhase { get; set; }
+    public string? ResourceType { get; set; }
+}
