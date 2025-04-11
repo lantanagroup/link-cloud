442a1998bef13f38e103542a66e1e85dec5b2dc4
MontaltoNick <nick.montalto@lantanagroup.com>
2025-04-11 13:49:49 -0500

    checkin

--- DotNet/DataAcquisition/Program.cs
+++ DotNet/DataAcquisition/Program.cs
@@ -349,7 +349,7 @@ static void RegisterServices(WebApplicationBuilder builder)
     var kafkaHealthOptions = new KafkaHealthCheckConfiguration(kafkaConnection, DataAcquisitionConstants.ServiceName).GetHealthCheckOptions();
 
     builder.Services.AddHealthChecks()
-        .AddDbContextCheck<DataAcquisitionDbContext>("Database")
+        .AddDbContextCheck<DataAcquisitionDbContext>(HealthCheckType.Database.ToString())
         .AddKafka(kafkaHealthOptions, HealthCheckType.Kafka.ToString());
 
     builder.Services.AddSingleton(TimeProvider.System);
