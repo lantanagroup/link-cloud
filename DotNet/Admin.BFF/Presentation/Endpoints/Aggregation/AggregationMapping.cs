6dfd8b874e8d8394f72421c3bc33d4bf04af9f13
MontaltoNick <nick.montalto@lantanagroup.com>
2025-04-11 10:12:58 -0500

    checkin

--- DotNet/Admin.BFF/Presentation/Endpoints/Aggregation/AggregationMapping.cs
+++ DotNet/Admin.BFF/Presentation/Endpoints/Aggregation/AggregationMapping.cs
@@ -1,5 +1,4 @@
 using LantanaGroup.Link.LinkAdmin.BFF.Presentation.Endpoints.Aggregation.Handlers.Report;
-using LantanaGroup.Link.LinkAdmin.BFF.Presentation.Endpoints.Aggregation.Handlers.Tenant;
 using LantanaGroup.Link.Shared.Application.Models.Report;
 using Link.Authorization.Infrastructure;
 using Microsoft.OpenApi.Models;
@@ -14,7 +13,7 @@ public static class AggregationMapping
         {
             Tags = new List<OpenApiTag> { new() { Name = "Service Aggregation" } }
         });
-        
+
         routes.MapGet("/reports/summaries", GetReportSummaries.Handle)
             .RequireAuthorization(LinkAuthorizationConstants.LinkBearerService.AuthenticatedUserPolicyName)
             .Produces<List<ScheduledReportListSummary>>()
@@ -27,29 +26,6 @@ public static class AggregationMapping
                 Description = "Retrieves a list of report summaries."
             });
 
-        routes.MapGet("/facility/adhocReportRequest", PostAdhocReportRequested.Handle)
-            .RequireAuthorization(LinkAuthorizationConstants.LinkBearerService.AuthenticatedUserPolicyName)
-            .Produces<HttpResponseMessage>()
-            .Produces(StatusCodes.Status401Unauthorized)
-            .Produces(StatusCodes.Status403Forbidden)
-            .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithOpenApi(x => new OpenApiOperation(x)
-            {
-                Summary = "Generate an AdhocReport For Facility",
-                Description = "Initiates the process of generating an AdHoc Report for a given Facility."
-            });
-
-        routes.MapGet("/facility/regenerateReportRequest", PostGenerateReportRequest.Handle)
-            .RequireAuthorization(LinkAuthorizationConstants.LinkBearerService.AuthenticatedUserPolicyName)
-            .Produces<HttpResponseMessage>()
-            .Produces(StatusCodes.Status401Unauthorized)
-            .Produces(StatusCodes.Status403Forbidden)
-            .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithOpenApi(x => new OpenApiOperation(x)
-            {
-                Summary = "Regenerate a previously scheduled Report For Facility",
-                Description = "Initiates the process of regenerating a previously scheduled Report for a given Facility."
-            });
         return routes;
     }
 }
