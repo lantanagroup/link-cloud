5583df1a77d643bca3efa729140f069fba4153ef
ariana.mihailescu <ariana.mihailescu@lantanagroup.com>
2025-04-22 13:16:38 -0400

    LNK-3064: Add Kafka Health Check for Measure Evaluation - added classed to Shared folder to make common functionality available to other services (Validation)

---
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/health/KafkaHealthCheckIndicator.java
@@ -0,0 +1,55 @@
+package com.lantanagroup.link.shared.health;
+
+import com.lantanagroup.link.shared.kafka.Properties;
+import com.lantanagroup.link.shared.kafka.Topics;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+@Component("kafka")
+public class KafkaHealthCheckIndicator implements HealthIndicator {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private final String serviceName;
+
+    public KafkaHealthCheckIndicator(KafkaTemplate<String, String> kafkaTemplate, @Value(" ${spring.application.name}") String serviceName) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.serviceName = serviceName;
+    }
+
+    @Override
+    public Health health() {
+        // Perform a health check on Kafka
+        boolean isKafkaHealthy = checkKafkaConnection();
+        if (isKafkaHealthy) {
+            return Health.up().withDetail("Kafka", "Available").build();
+        } else {
+            return Health.down().withDetail("Kafka", "Unavailable").build();
+        }
+    }
+
+    private boolean checkKafkaConnection() {
+        try {
+            // format the message to send to Health Kafka topic
+            ZonedDateTime utcDate = ZonedDateTime.now(ZoneId.of("UTC"));
+            String formattedDate = utcDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            String message = String.format("Service health check on %s (UTC)", formattedDate);
+
+            // the send() call itself is non-blocking so we need a get to force the program to wait till the broker acknowledges the message or throws an exception if Kafka is down or the timeout expires
+            // the timeout on get should be >= MAX_BLOCK_MS_CONFIG configured on the ProducerConfig properties of KafkaTemplate; add another 1000 millis to the configured MAX_BLOCK_MS_CONFIG property
+            long getTimeOut  = Properties.MAX_BLOCK_MS_CONFIG + 1000; // in millis
+            kafkaTemplate.send(Topics.SERVICE_HEALTH_CHECK, serviceName, message).get(getTimeOut, TimeUnit.MILLISECONDS);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
