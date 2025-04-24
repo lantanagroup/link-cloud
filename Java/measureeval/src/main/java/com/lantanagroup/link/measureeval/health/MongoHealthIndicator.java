a7cef5ae0b0584cfe90b07b305e80e13c86c2ac5
ariana.mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2025-04-24 16:36:32 -0400

    LNK-3694: Add Database Health Check for Measure Evaluation and JUnit test

--- Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/health/MongoHealthIndicator.java
+++ Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/health/MongoHealthIndicator.java
@@ -1,4 +1,4 @@
-package com.lantanagroup.link.shared.health;
+package com.lantanagroup.link.measureeval.health;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
