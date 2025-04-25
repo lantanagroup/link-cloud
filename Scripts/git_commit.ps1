c100c136d1476913721be160ac78b8d1508d5ba7
Keith Kissal <99497673+kissalk@users.noreply.github.com>
2025-04-25 10:02:59 -0400

    addings vars

--- Scripts/git_commit.ps1
+++ Scripts/git_commit.ps1
@@ -7,10 +7,13 @@ $serviceName = $env:SERVICENAME
 # Load the JSON file
 $json = Get-Content $jsonPath -Raw | ConvertFrom-Json
 
+# Gathers the current GitCommit value
 $old = $json.ServiceInformation.GitCommit
 
+# Updates the GitCommit value
 $new = "$env:GIT_COMMIT"
 
 (Get-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json") | ForEach-Object { $_ -replace $old, $new } | Set-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json"
 
+# ECHOs the Appsettings.json value
 Get-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json"
