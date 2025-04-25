a247fa0ba0f8c6139c3d5dc2d5685d97cbcc7ea9
Keith Kissal <99497673+kissalk@users.noreply.github.com>
2025-04-25 09:27:56 -0400

    udpated script

--- Scripts/git_commit.ps1
+++ Scripts/git_commit.ps1
@@ -1,5 +1,5 @@
 # Path to your JSON file
-$jsonPath = "$(Build.SourcesDirectory)/DotNet/Account/appsettings.json"
+$jsonPath = "$env:BUILD_SOURCESDIRECTORY/DotNet/Account/appsettings.json"
 
 # Load the JSON file
 $json = Get-Content $jsonPath -Raw | ConvertFrom-Json
@@ -8,6 +8,6 @@ $old = $json.ServiceInformation.GitCommit
 
 $new = "12344"
 
-(Get-Content -Path "$(Build.SourcesDirectory)/DotNet/Account/appsettings.json") | ForEach-Object { $_ -replace $old, $new } | Set-Content -Path "$(Build.SourcesDirectory)/DotNet/Account/appsettings.json"
+(Get-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/Account/appsettings.json") | ForEach-Object { $_ -replace $old, $new } | Set-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/Account/appsettings.json"
 
 Write-Host $json
