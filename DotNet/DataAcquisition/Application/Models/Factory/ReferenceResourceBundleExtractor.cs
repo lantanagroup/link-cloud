b47ba1e97eddbbf09e26932221f00cba916a32d1
Steven Williams <steven.williams@lantanagroup.com>
2025-05-27 11:23:50 -0400

    Extract references by resource type
    The previous implementation only recognized references whose element
    name matched a desired resource type.

--- DotNet/DataAcquisition/Application/Models/Factory/ReferenceResourceBundleExtractor.cs
+++ DotNet/DataAcquisition/Application/Models/Factory/ReferenceResourceBundleExtractor.cs
@@ -1,4 +1,5 @@
 ﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
 
 namespace LantanaGroup.Link.DataAcquisition.Application.Models.Factory;
 
@@ -46,9 +47,13 @@ public class ReferenceResourceBundleExtractor
 
         foreach (var property in ancestor.NamedChildren)
         {
-            if (validResourceTypes.Contains(property.ElementName, StringComparer.InvariantCultureIgnoreCase))
+            if (property.Value is ResourceReference reference)
             {
-                results.Add(property.Value);
+                ResourceIdentity identity = new(reference.Reference);
+                if (validResourceTypes.Contains(identity.ResourceType))
+                {
+                    results.Add(reference);
+                }
             }
 
             Walk(property.Value, results, validResourceTypes);
