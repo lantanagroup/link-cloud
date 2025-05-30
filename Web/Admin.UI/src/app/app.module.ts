1a017689354f8bcac2d499bce2ca02e95030ff71
jreynolds <jace.reynolds@fjorgedigital.com>
2025-05-29 19:17:20 -0500

    icon component, subnav component, import both into app, add subnav component to app scaffolding to test

--- Web/Admin.UI/src/app/app.module.ts
+++ Web/Admin.UI/src/app/app.module.ts
@@ -1,31 +1,34 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
-import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {ThemePickerComponent} from './components/core/theme-picker/theme-picker.component';
-import {StyleManagerService} from './services/style-manager-service';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatNativeDateModule} from '@angular/material/core';
-import {LoadingIndicatorComponent} from './components/core/loading-indicator/loading-indicator.component';
-import {HttpInterceptorProviders} from './interceptors/interceptor.barrel';
-import {AppConfigService} from './services/app-config.service';
-import {AuthenticationService} from './services/security/authentication.service';
-import {LinkNavBarComponent} from './components/core/link-nav-bar/link-nav-bar.component';
-import {BreadcrumbComponent} from "./components/core/breadcrumb/breadcrumb.component";
-import {FooterComponent} from "./components/core/footer/footer.component";
-import {ToastrModule} from 'ngx-toastr';
-import {AuthConfig, OAuthModule, OAuthModuleConfig, OAuthService} from "angular-oauth2-oidc";
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { AuthConfig, OAuthModule, OAuthModuleConfig, OAuthService } from "angular-oauth2-oidc";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
-export function initConfig(appConfig: AppConfigService, oauthService:OAuthService, authService: AuthenticationService, oauthModuleConfig: OAuthModuleConfig) {
+import { AppComponent } from './app.component';
+import { AppConfigService } from './services/app-config.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthenticationService } from './services/security/authentication.service';
+import { BreadcrumbComponent } from "./components/core/breadcrumb/breadcrumb.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FooterComponent } from "./components/core/footer/footer.component";
+import { HttpInterceptorProviders } from './interceptors/interceptor.barrel';
+import { LayoutModule } from '@angular/cdk/layout';
+import { LinkAdminSubnavBarComponent } from './components/core/link-admin-subnav-bar/link-admin-subnav-bar.component';
+import { LinkNavBarComponent } from './components/core/link-nav-bar/link-nav-bar.component';
+import { LoadingIndicatorComponent } from './components/core/loading-indicator/loading-indicator.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { StyleManagerService } from './services/style-manager-service';
+import { ThemePickerComponent } from './components/core/theme-picker/theme-picker.component';
+import { ToastrModule } from 'ngx-toastr';
+import { VdIconComponent } from './components/core/vd-icon/vd-icon.component';
+
+export function initConfig(appConfig: AppConfigService, oauthService: OAuthService, authService: AuthenticationService, oauthModuleConfig: OAuthModuleConfig) {
   const configPromise = appConfig.loadConfig()
     .then(async config => {
       if (config?.oauth2?.enabled) {
@@ -56,7 +59,8 @@ export function initConfig(appConfig: AppConfigService, oauthService:OAuthServic
   return () => configPromise;
 }
 
-@NgModule({ declarations: [
+@NgModule({
+  declarations: [
     AppComponent
   ],
   bootstrap: [AppComponent],
@@ -78,6 +82,8 @@ export function initConfig(appConfig: AppConfigService, oauthService:OAuthServic
     LinkNavBarComponent,
     BreadcrumbComponent,
     FooterComponent,
+    LinkAdminSubnavBarComponent,
+    VdIconComponent,
     ToastrModule.forRoot(),
     OAuthModule.forRoot({
       resourceServer: {
@@ -85,7 +91,7 @@ export function initConfig(appConfig: AppConfigService, oauthService:OAuthServic
         sendAccessToken: true
       }
     })
-],
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
