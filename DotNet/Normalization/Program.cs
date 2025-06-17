a46483426875c515a878b874058be2a5dd6c3f09
MontaltoNick <nick.montalto@lantanagroup.com>
2025-06-17 13:52:49 -0500

    Remove MediatR, changed to Scoped services.

--- DotNet/Normalization/Program.cs
+++ DotNet/Normalization/Program.cs
@@ -188,17 +188,15 @@ static void RegisterServices(WebApplicationBuilder builder)
                     .Enrich.With<ActivityEnricher>()
                     .CreateLogger();
 
-    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
     //Managers
-    builder.Services.AddTransient<IDatabase, Database>();
-    builder.Services.AddTransient<IOperationManager, OperationManager>();
-    builder.Services.AddTransient<IResourceManager, ResourceManager>();
-    builder.Services.AddTransient<IVendorOperationPresetManager, VendorOperationPresetManager>();
-    builder.Services.AddTransient<IOperationQueries, OperationQueries>(); 
-    builder.Services.AddTransient<IOperationSequenceQueries, OperationSequenceQueries>();
-    builder.Services.AddTransient<IVendorQueries, VendorQueries>();
-    builder.Services.AddTransient<IResourceQueries, ResourceQueries>();
+    builder.Services.AddScoped<IDatabase, Database>();
+    builder.Services.AddScoped<IOperationManager, OperationManager>();
+    builder.Services.AddScoped<IResourceManager, ResourceManager>();
+    builder.Services.AddScoped<IVendorOperationPresetManager, VendorOperationPresetManager>();
+    builder.Services.AddScoped<IOperationQueries, OperationQueries>(); 
+    builder.Services.AddScoped<IOperationSequenceQueries, OperationSequenceQueries>();
+    builder.Services.AddScoped<IVendorQueries, VendorQueries>();
+    builder.Services.AddScoped<IResourceQueries, ResourceQueries>();
 
     builder.Services.AddControllers()
     .AddJsonOptions(options =>
