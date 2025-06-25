753a41222dcab3b658cf060724d718f7c5769310
MontaltoNick <nick.montalto@lantanagroup.com>
2025-06-25 14:14:35 -0500

    More Robust FhirPath Validation

--- DotNet/Normalization/Application/Services/Operations/CodeMapOperatonService.cs
+++ DotNet/Normalization/Application/Services/Operations/CodeMapOperatonService.cs
@@ -12,17 +12,16 @@ namespace LantanaGroup.Link.Normalization.Application.Services.Operations
         {
         }
 
-        protected override DomainResource ExecuteOperation(CodeMapOperation operation, DomainResource resource)
+        protected override OperationResult ExecuteOperation(CodeMapOperation operation, DomainResource resource)
         {
-            if (!OperationServiceHelper.ValidateFhirPath(operation.FhirPath, out var validationError, Logger))
+            if (!OperationServiceHelper.ValidateFhirPath(operation.FhirPath, resource, out var validationError, Logger))
             {
-                Logger.LogWarning("Invalid FHIRPath {FhirPath} for operation {OperationName}: {ErrorMessage}", operation.FhirPath, operation.Name, validationError);
-                return resource;
+                return OperationResult.Failure($"Invalid FHIRPath {operation.FhirPath} for operation {operation.Name}: {validationError}", resource);
             }
 
             var source = resource.Select(operation.FhirPath).FirstOrDefault();
             if (source == null)
-                return resource;
+                return OperationResult.Failure($"Nothing found at {operation.FhirPath}", resource);
 
             if (source is Coding coding)
             {
@@ -38,7 +37,7 @@ namespace LantanaGroup.Link.Normalization.Application.Services.Operations
                 Logger.LogWarning("Unsupported source type {SourceType} for FHIRPath {FhirPath} in operation {OperationName}.", source.GetType().Name, operation.FhirPath, operation.Name);
             }
 
-            return resource;
+            return OperationResult.Success(resource);
         }
 
         private void UpdateCoding(Coding coding, List<CodeSystemMap> codeSystemMaps)
