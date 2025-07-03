717dad06053ef66d97a7b18f537eb330820ef8f9
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-07-02 23:23:53 -0500

    Update ReferenceResourceService.cs
    * add query type assignment

--- DotNet/DataAcquisition.Domain/Application/Services/ReferenceResourceService.cs
+++ DotNet/DataAcquisition.Domain/Application/Services/ReferenceResourceService.cs
@@ -130,6 +130,8 @@ public class ReferenceResourceService : IReferenceResourceService
         //group refResources by type
         var groupedRefResources = refResources.Where(r => r.Url != null).GroupBy(r => r.Url.ToString().Split('/')[0]).ToList();
 
+        _logger.LogInformation("Processing {Count} reference resources for log with ID: {LogId}", groupedRefResources.Sum(g => g.Count()), log.Id);
+
         foreach (var refResourcesTypeGroup in groupedRefResources)
         {
             var resourceType = refResourcesTypeGroup.Key;
@@ -146,34 +148,6 @@ public class ReferenceResourceService : IReferenceResourceService
             }
 
             var existingLog = await _dataAcquisitionLogQueries.GetLogByFacilityIdAndReportTrackingIdAndResourceType(log.FacilityId, log.ReportTrackingId, resourceType, log.CorrelationId, cancellationToken);
-
-            if (existingLog == null)
-            {
-
-                var refResourcesTypes = //get all reference types from every log.FhirQuery and combine into 1 list
-                    log.FhirQuery.SelectMany(x => x.ResourceReferenceTypes)
-                        .Select(x => new ResourceReferenceType
-                        {
-                            ResourceType = x.ResourceType,
-                            FacilityId = log.FacilityId,
-                            QueryPhase = log.QueryPhase.Value,
-                        })
-                        .DistinctBy(x => x.ResourceType)
-                        .ToList();
-
-
-                //create new log entry for resource
-                var newFhirQueries = new List<FhirQuery>
-                    {
-                        new FhirQuery
-                        {
-                            FacilityId = log.FacilityId,
-                            ResourceReferenceTypes = refResourcesTypes,
-                            MeasureId = log.ScheduledReport.ReportTypes.FirstOrDefault(),
-                        }
-                    };
-                existingLog = CreateDataAcquisitionLog(log, resourceType, refResourcesTypes, newFhirQueries);
-            }
 
             // Filter out references that already exist
             var refResourceListForTypeFiltered = refResourcesListDeDuped.Where(x => !existingRefIds.Contains(x.Reference.SplitReference())).ToList();
@@ -218,9 +192,9 @@ public class ReferenceResourceService : IReferenceResourceService
                                 },
                             }
                         };
-                    
+
                     existingLog = CreateDataAcquisitionLog(log, refResourcesTypeGroup.Key, refResourcesTypes, newFhirQueries);
-                    
+
 
                     //add the log entry
                     await _dataAcquisitionLogManager.CreateAsync(existingLog, cancellationToken);
@@ -255,7 +229,7 @@ public class ReferenceResourceService : IReferenceResourceService
                                 }
                             }
                         };
-                        //existingLog.FhirQuery.Add(fhirQuery);
+
                         await _fhirQueryMananger.AddAsync(fhirQuery, cancellationToken);
                     }
                 }
@@ -292,6 +266,7 @@ public class ReferenceResourceService : IReferenceResourceService
             ReportableEvent = log.ReportableEvent,
             FhirVersion = log.FhirVersion,
             QueryPhase = log.QueryPhase,
+            QueryType = FhirQueryType.Search,
             Status = RequestStatus.Pending,
             TimeZone = "UTC",
             ScheduledReport = log.ScheduledReport,
