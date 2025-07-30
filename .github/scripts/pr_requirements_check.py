1e9c50c369e94c02953f714a5ee30ea7c27fb536
Stephen McGeown <39219436+sdmcgeown@users.noreply.github.com>
2025-07-29 22:54:37 -0700

    debug

--- .github/scripts/pr_requirements_check.py
+++ .github/scripts/pr_requirements_check.py
@@ -20,10 +20,6 @@ def extract_sections(text):
 def main():
     pr_title = os.environ.get('PR_TITLE', '')
     pr_body = os.environ.get('PR_BODY', '')
-    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-    print(f'PR_TITLE: {pr_title}')
-    print(f'PR_BODY: {pr_body}')
-    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
     if not pr_title:
         fail('PR title is missing! Please provide a valid PR title.')
     if not pr_body:
@@ -44,23 +40,14 @@ def main():
     except Exception as e:
         fail(f'Could not read pull_request_template.md for PR description validation: {e}')
 
-    print('oooooooooooooooooooooooooooooooooooooooo')
-    print(f'template_path: {template_path}')
-    print(f'template_content: {template_content}')
-    print('oooooooooooooooooooooooooooooooooooooooo')
-
     template_sections = extract_sections(template_content)
-    print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
-    print(f'template_sections: {template_sections}')
-    print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
     pr_sections = extract_sections(pr_body or '')
 
     if not template_sections:
-        print('Pull request template is empty or not formatted correctly.')
+        fail('Pull request template is empty or not formatted correctly.')
     if not pr_sections:
-        print('Pull request description is empty. Please fill out the required sections.')
+        fail('Pull request description is empty. Please fill out the required sections.')
 
-    print (f'Length of template sections: {len(template_sections)}')
     incomplete_sections = []
     for section, template_text in template_sections.items():
         pr_text = pr_sections.get(section, '')
