369d8986ae9de132e958c41adaeb61a76177a313
Steven Williams <steven.williams@lantanagroup.com>
2025-08-05 10:09:07 -0400

    Fix expected submission files

--- Tests/BackendE2ETests/AdhocReportingSmokeTest.cs
+++ Tests/BackendE2ETests/AdhocReportingSmokeTest.cs
@@ -114,30 +114,14 @@ public sealed class AdhocReportingSmokeTest(ITestOutputHelper output) : IAsyncLi
         // Download the report
         var downloadedResources = await this.DownloadReport(reportId);
 
-        // Confirm that there is a file called "sending-org.json"
-        Assert.True(downloadedResources.ContainsKey("sending-organization.json"), $"Expected report to include sending-org.json but it was not");
+        // Confirm that there is a file called "manifest.ndjson"
+        Assert.True(downloadedResources.ContainsKey("manifest.ndjson"), $"Expected report to include manifest.ndjson but it was not");
         // TODO: Validate that it is correct
 
-        // Confirm that there is a file called "patient-list.json"
-        Assert.True(downloadedResources.ContainsKey("patient-list.json"), $"Expected report to include patient-list.json but it was not");
-        // TODO: Validate that it is correct
-
-        // Confirm that there is a file called "sending-device.json"
-        Assert.True(downloadedResources.ContainsKey("sending-device.json"), $"Expected report to include sending-device.json but it was not");
-        // TODO: Validate that it is correct
-
-        // Confirm that there is a file called "aggregate-ACHM.json"
-        Assert.True(downloadedResources.ContainsKey("aggregate-ACHM.json"), $"Expected report to include aggregate-ACHM.json but it was not");
-        // TODO: Validate that it is correct
-
-        // Confirm that there is a file called "other-resources.json"
-        Assert.True(downloadedResources.ContainsKey("other-resources.json"), $"Expected report to include other-resources.json but it was not");
-        // TODO: Validate that it is correct
-
-        // Confirm that there is a file called "patient-{patientId}.json"
+        // Confirm that there is a file called "patient-{patientId}.ndjson"
         foreach (var patientId in TestConfig.AdhocReportingSmokeTestConfig.PatientIds)
         {
-            Assert.True(downloadedResources.ContainsKey($"patient-{patientId}.json"), $"Expected report to include patient-{patientId}.json but it was not");
+            Assert.True(downloadedResources.ContainsKey($"patient-{patientId}.ndjson"), $"Expected report to include patient-{patientId}.ndjson but it was not");
             // TODO: Validate that it is correct
         }
 
