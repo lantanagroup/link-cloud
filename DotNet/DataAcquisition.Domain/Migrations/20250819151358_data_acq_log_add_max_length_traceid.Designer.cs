2bcedbc52a45d2bf396cfc27c951f92a3a9148cc
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-08-19 10:15:04 -0500

    LNK-4167: add max length to trace id

--- DotNet/DataAcquisition.Domain/Migrations/20250819151358_data_acq_log_add_max_length_traceid.Designer.cs
+++ DotNet/DataAcquisition.Domain/Migrations/20250819151358_data_acq_log_add_max_length_traceid.Designer.cs
@@ -4,6 +4,7 @@ using LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAcquisition.Domain.Migrations
 {
     [DbContext(typeof(DataAcquisitionDbContext))]
-    partial class DataAcquisitionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250819151358_data_acq_log_add_max_length_traceid")]
+    partial class data_acq_log_add_max_length_traceid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
