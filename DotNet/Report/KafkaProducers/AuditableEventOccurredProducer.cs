3dde03cc00628f39d6d1351a6ccb6ba25ae2664b
Steven Williams <steven.williams@lantanagroup.com>
2025-08-19 07:53:47 -0400

    Add `AuditableEventOccurred` producer

---
+++ DotNet/Report/KafkaProducers/AuditableEventOccurredProducer.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using LantanaGroup.Link.Report.Settings;
+using LantanaGroup.Link.Shared.Application.Models;
+using LantanaGroup.Link.Shared.Application.Models.Kafka;
+using System.Text;
+
+namespace LantanaGroup.Link.Report.KafkaProducers
+{
+    public class AuditableEventOccurredProducer(
+        ILogger<AuditableEventOccurredProducer> _logger,
+        IProducer<string, object> _producer)
+    {
+        public async Task ProduceAsync(AuditEventMessage model, CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("Producing audit event: {}", model.Notes);
+            try
+            {
+                Headers headers = [];
+                if (!string.IsNullOrEmpty(model.CorrelationId))
+                {
+                    headers.Add("X-Correlation-Id", Encoding.ASCII.GetBytes(model.CorrelationId));
+                }
+                model.ServiceName = ReportConstants.ServiceName;
+                await _producer.ProduceAsync(nameof(KafkaTopic.AuditableEventOccurred), new Message<string, object>
+                {
+                    Headers = headers,
+                    Key = model.FacilityId ?? "",
+                    Value = model
+                }, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to produce audit event.");
+            }
+        }
+    }
+}
