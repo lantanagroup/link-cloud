2ed3e77f14b29fb501208d09713efd361021b1c8
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-09-08 21:30:01 -0500

    Update EpicAuth.cs

--- DotNet/DataAcquisition.Domain/Application/Services/Auth/EpicAuth.cs
+++ DotNet/DataAcquisition.Domain/Application/Services/Auth/EpicAuth.cs
@@ -1,5 +1,4 @@
 ﻿using LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Models;
-using LantanaGroup.Link.DataAcquisition.Domain.Services.Interfaces;
 using LantanaGroup.Link.DataAcquisition.Domain.Settings;
 using LantanaGroup.Link.Shared.Application.Interfaces;
 using LantanaGroup.Link.Shared.Application.Models.Configs;
