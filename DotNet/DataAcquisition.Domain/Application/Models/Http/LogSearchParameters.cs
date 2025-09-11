7f9b6f43e5f26a97c1b6c2309ffd3c3dd7e924c4
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-09-11 15:34:11 -0500

    Update LogSearchParameters.cs

--- DotNet/DataAcquisition.Domain/Application/Models/Http/LogSearchParameters.cs
+++ DotNet/DataAcquisition.Domain/Application/Models/Http/LogSearchParameters.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Http;
 public class LogSearchParameters : GenericLogSearchParameters
 {
-    public string FacilityId { get; set; }
+    public string? FacilityId { get; set; }
     public string? PatientId { get; set; }
     public string? ReportId { get; set; }
     public string? ResourceId { get; set; }
