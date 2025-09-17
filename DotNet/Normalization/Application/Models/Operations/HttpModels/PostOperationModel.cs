a00417913f5741de1b5d2955349324caebfea3b3
Daniel Vargas <116004423+dvargaslantana@users.noreply.github.com>
2025-09-17 14:44:48 -0500

    Initial Checkin. Added most of the code needed to accept the new operation. Added functionality to POST the new operation via REST. Added initial copy Location operation logic

--- DotNet/Normalization/Application/Models/Operations/HttpModels/PostOperationModel.cs
+++ DotNet/Normalization/Application/Models/Operations/HttpModels/PostOperationModel.cs
@@ -6,7 +6,7 @@ using System.Runtime.Serialization;
 namespace LantanaGroup.Link.Normalization.Application.Models.Operations.HttpModels
 {
     [ExcludeFromCodeCoverage]
-    public class PostOperationModel()
+    public class PostOperationModel
     {
         [Required, DataMember]
         public List<string> ResourceTypes { get; set; } = new List<string>();
@@ -15,6 +15,19 @@ namespace LantanaGroup.Link.Normalization.Application.Models.Operations.HttpMode
         [DataMember]
         public string? FacilityId { get; set; } = null;
         [DataMember(IsRequired = false)]
-        public List<Guid>? VendorIds { get; set; }   
+        public List<Guid>? VendorIds { get; set; }
+
+        public PostOperationModel(List<string> resourceTypes, IOperation operation, string? facilityId, List<Guid>? vendorIds)
+        {
+            ResourceTypes = resourceTypes ?? new List<string>();
+            Operation = operation;
+            FacilityId = facilityId;
+            VendorIds = vendorIds;
+
+            if (this.Operation.OperationType == OperationType.CopyLocation)
+            {
+                this.ResourceTypes.Add("Location");
+            }
+        }
     }
 }
---
+++ DotNet/Normalization/Application/Services/Operations/CopyLocationOperation.cs
@@ -0,0 +1,37 @@
+﻿using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.Model;
+using Hl7.FhirPath;
+using LantanaGroup.Link.Normalization.Application.Models.Operations;
+using LantanaGroup.Link.Normalization.Application.Operations;
+using LantanaGroup.Link.Normalization.Application.Services.FhirPathValidation;
+using System.Collections;
+
+namespace LantanaGroup.Link.Normalization.Application.Services.Operations
+{
+    public class CopyLocationOperationService : BaseOperationService<CopyLocationOperation>
+    {
+        public CopyLocationOperationService(ILogger<CopyLocationOperationService> logger, TimeSpan? operationTimeout = null)
+            : base(logger, operationTimeout)
+        {
+        }
+
+        protected override async Task<OperationResult> ExecuteOperation(CopyLocationOperation operation, DomainResource resource)
+        {
+            if (resource is not Location) {
+                return OperationResult.Failure($"Resource must be a Location");
+            }
+
+            if (((Location)resource).Type == null)
+            {
+                ((Location)resource).Type = new List<CodeableConcept>();
+            }
+
+            foreach (var identifier in ((Location)resource).Identifier) {
+                CodeableConcept codeableConcept = new(identifier.System, identifier.Value);
+                ((Location)resource).Type.Add(codeableConcept);
+            }
+
+            return OperationResult.Success(resource);
+        }
+    }
+}
