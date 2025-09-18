98351dbcde86339ce5805e29bb9dcfc62f57d153
ariana.mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2025-09-18 17:02:00 -0400

    LNK-4291: Added security headers including CSP

--- Java/shared/src/main/java/com/lantanagroup/link/shared/security/SecurityHelper.java
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/security/SecurityHelper.java
@@ -41,29 +41,28 @@ public class SecurityHelper {
         return http.build();
     }
 */
-
   public static SecurityFilterChain build(HttpSecurity http, JwtAuthenticationEntryPoint point, JwtAuthenticationFilter authFilter) throws Exception {
-      http.csrf(AbstractHttpConfigurer::disable).addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-              .authorizeRequests().
-              requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-              .requestMatchers(HttpMethod.GET, "/health").permitAll()
-              .requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll()
-              .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
-              //requestMatchers("/api/**").access("hasRole('LinkUser') and hasAuthority('IsLinkAdmin')").and().exceptionHandling(ex -> ex.authenticationEntryPoint(point)  - done in the specific end points using annotations for more granular control
-              .requestMatchers("/api/**").authenticated().and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
-              .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-              .headers(headers -> headers
-                      .contentSecurityPolicy(csp -> csp
-                              .policyDirectives(
-                                      "default-src 'self'; " +
-                                      "script-src 'self'; " +
-                                      "style-src 'self' 'unsafe-inline'; " +
-                                      "img-src 'self' data:;" +
-                                      "connect-src 'self';"
-                              )
-                      )
-              );
-      return http.build();
+    http.csrf(AbstractHttpConfigurer::disable).addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests().
+            requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/health").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+            //requestMatchers("/api/**").access("hasRole('LinkUser') and hasAuthority('IsLinkAdmin')").and().exceptionHandling(ex -> ex.authenticationEntryPoint(point)  - done in the specific end points using annotations for more granular control
+            .requestMatchers("/api/**").authenticated().and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .headers(headers -> headers
+                    .contentSecurityPolicy(csp -> csp
+                            .policyDirectives(
+                                    "default-src 'self'; " +
+                                    "script-src 'self'; " +
+                                    "style-src 'self' 'unsafe-inline'; " +
+                                    "img-src 'self' data:;" +
+                                    "connect-src 'self';"
+                            )
+                    )
+            );
+    return http.build();
   }
 
 
