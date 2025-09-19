f5d960cc4e64505b3fd0ef0121a1eb8631bc8e29
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2025-09-19 09:44:25 -0700

    Adding option for "build" to set_service_info.py Updating pipelines to set build number to pipeline's Build.BuildNumber

--- Scripts/set_service_info.py
+++ Scripts/set_service_info.py
@@ -3,7 +3,7 @@ import sys
 import json
 import yaml
 
-def update_java_config(yaml_path, commit, product_version, test_mode=False):
+def update_java_config(yaml_path, commit, build=None, product_version=None, test_mode=False):
     with open(yaml_path, "r", encoding="utf-8") as f:
         try:
             data = yaml.safe_load(f) or {}
@@ -17,6 +17,9 @@ def update_java_config(yaml_path, commit, product_version, test_mode=False):
     changed = False
     if commit is not None:
         data["service-information"]["commit"] = commit
+        changed = True
+    if build is not None:
+        data["service-information"]["build"] = build
         changed = True
     if product_version is not None:
         data["service-information"]["product-version"] = product_version
@@ -35,7 +38,7 @@ def update_java_config(yaml_path, commit, product_version, test_mode=False):
 
     print(f"Updated {yaml_path} with service-information.commit = {commit}")
 
-def update_dotnet_config(json_path, commit, product_version, test_mode=False):
+def update_dotnet_config(json_path, commit, build=None, product_version=None, test_mode=False):
     with open(json_path, "r", encoding="utf-8") as f:
         try:
             data = json.load(f)
@@ -49,6 +52,9 @@ def update_dotnet_config(json_path, commit, product_version, test_mode=False):
     changed = False
     if commit is not None:
         data["ServiceInformation"]["Commit"] = commit
+        changed = True
+    if build is not None:
+        data["ServiceInformation"]["Build"] = build
         changed = True
     if product_version is not None:
         data["ServiceInformation"]["ProductVersion"] = product_version
@@ -67,14 +73,14 @@ def update_dotnet_config(json_path, commit, product_version, test_mode=False):
 
     print(f"Updated {json_path} with ServiceInformation.Commit = {commit}")
 
-def update_commit(directory, service_name, commit, product_version=None, test_mode=False):
+def update_commit(directory, service_name, commit, product_version=None, build=None, test_mode=False):
     json_path = os.path.join(directory, service_name, "appsettings.json")
     yaml_path = os.path.join(directory, service_name, "src", "main", "resources", "application.yml")
 
     if os.path.exists(json_path):
-        update_dotnet_config(json_path, commit, product_version, test_mode)
+        update_dotnet_config(json_path, commit, product_version, build, test_mode)
     elif os.path.exists(yaml_path):
-        update_java_config(yaml_path, commit, product_version, test_mode)
+        update_java_config(yaml_path, commit, product_version, build, test_mode)
     else:
         print(f"ERROR: Neither appsettings.json at {json_path} nor application.yml at {yaml_path} found")
         sys.exit(1)
@@ -82,22 +88,23 @@ def update_commit(directory, service_name, commit, product_version=None, test_mo
 
 def get_param(args, index, env_var):
     if len(args) > index:
-        return args[index]
+        return None if args[index] == "None" else args[index]
     return os.environ.get(env_var)
 
 if __name__ == "__main__":
     directory = get_param(sys.argv[1:], 0, 'BUILD_SOURCESDIRECTORY')
     service_name = get_param(sys.argv[1:], 1, 'SERVICENAME')
     commit = get_param(sys.argv[1:], 2, 'GIT_COMMIT')
-    product_version = get_param(sys.argv[1:], 3, 'PRODUCT_VERSION')
-    test_mode = get_param(sys.argv[1:], 4, 'TEST_MODE') == 'true'
+    build = get_param(sys.argv[1:], 3, 'BUILD_NUMBER')
+    product_version = get_param(sys.argv[1:], 4, 'PRODUCT_VERSION')
+    test_mode = get_param(sys.argv[1:], 5, 'TEST_MODE') == 'true'
 
     if not all([directory, service_name, commit]):
         print("Error: Missing required parameters.")
         print("Provide either command line arguments:")
-        print("  python update_commit.py <Directory> <ServiceName> <Commit> <ProductVersion> <TestMode?>")
+        print("  python update_commit.py <Directory> <ServiceName> <Commit> <Build> <ProductVersion> <TestMode?>")
         print("Or environment variables:")
-        print("  BUILD_SOURCESDIRECTORY, SERVICENAME, GIT_COMMIT, PRODUCT_VERSION TEST_MODE")
+        print("  BUILD_SOURCESDIRECTORY, SERVICENAME, GIT_COMMIT, BUILD_NUMBER, PRODUCT_VERSION, TEST_MODE")
         sys.exit(1)
 
-    update_commit(directory, service_name, commit, product_version, test_mode)
+    update_commit(directory, service_name, commit, build, product_version, test_mode)
