39eead6a51859ee037a77f185f8b4765f3c39611
ariana.mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2025-09-24 11:32:18 -0400

    LNK-4226: Added more logging for deleting consumer groups, retry on errors from the broker; added SSL when calling AdminClientConfig

--- DotNet/Admin.BFF/Application/Commands/Integration/KafkaConsumerManager.cs
+++ DotNet/Admin.BFF/Application/Commands/Integration/KafkaConsumerManager.cs
@@ -233,19 +233,17 @@ namespace LantanaGroup.Link.LinkAdmin.BFF.Application.Commands.Integration
             // remove only consumers for that reprtTrackingId
             RemoveConsumersBasedOnReportTrackingId(_consumers, reportTrackingId);
 
-            // Wait a short time to let the broker mark the group as empty
-            await Task.Delay(500);
 
-            bool deleted = await DeleteConsumerGroupAsync(string.Join(", ", _kafkaConnection.BootstrapServers), "Dynamic:" + reportTrackingId);
+            bool deleted = await DeleteConsumerGroupAsync(_kafkaConnection, "Dynamic:" + reportTrackingId);
 
             _logger.LogInformation("Consumer group {ReportTrackingId} deleted: {Deleted}", reportTrackingId, deleted);
 
         }
 
 
-        public async Task<bool> DeleteConsumerGroupAsync(string bootstrapServers, string groupId, CancellationToken cancellationToken = default)
+        public async Task<bool> DeleteConsumerGroupAsync(KafkaConnection conn, string groupId, CancellationToken cancellationToken = default)
         {
-            var config = new AdminClientConfig { BootstrapServers = bootstrapServers };
+            var config = new AdminClientConfig { BootstrapServers = string.Join(", ", _kafkaConnection.BootstrapServers), SecurityProtocol = SecurityProtocol.SaslSsl, SaslMechanism = SaslMechanism.Plain, SaslUsername = conn.SaslUsername, SaslPassword = conn.SaslPassword };
 
             int delaySeconds = 3; // Start with 3 second
             int maxDelaySeconds = 120;  // Cap to avoid very long delays
@@ -268,7 +266,7 @@ namespace LantanaGroup.Link.LinkAdmin.BFF.Application.Commands.Integration
                         var completed = await Task.WhenAny(describeTask, Task.Delay(Timeout.Infinite, cancellationToken));
                         if (completed != describeTask)
                         {
-                            _logger.LogError("Describe consumer group cancellaton error.");
+                            _logger.LogError("Describe consumer group cancellation error.");
                             throw new OperationCanceledException(cancellationToken);
                         }
                         groupDescription = await describeTask;
