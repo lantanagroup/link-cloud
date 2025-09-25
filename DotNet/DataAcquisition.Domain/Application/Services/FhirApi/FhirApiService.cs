fb92fcf72c2cc5f9640378584c8e97272b1c6fae
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-09-25 10:40:32 -0500

    Fix coderabbit findings

--- DotNet/DataAcquisition.Domain/Application/Services/FhirApi/FhirApiService.cs
+++ DotNet/DataAcquisition.Domain/Application/Services/FhirApi/FhirApiService.cs
@@ -302,8 +302,6 @@ public class FhirApiService : IFhirApiService
     {
         if (resource == null) throw new ArgumentNullException(nameof(resource));
 
-        InsertDateExtension((DomainResource)resource);
-
         //get existing reference resource record
         var existingReference = await _referenceResourceManager.GetByResourceIdAndFacilityId(resource.Id, log.FacilityId, cancellationToken);
 
@@ -365,16 +363,10 @@ public class FhirApiService : IFhirApiService
         if(resource == null)
             throw new ArgumentNullException(nameof(resource));
 
-        if(resource.Meta == null)
-        {
-            resource.Meta = new Meta();
-            resource.Meta.Extension = new List<Extension> { };
-        }
+        resource.Meta ??= new Meta();
+        resource.Meta.Extension ??= new List<Extension>();
 
-        if(resource.Meta.Extension == null)
-            resource.Meta.Extension = new List<Extension> { };
-
-        if (!resource.Extension.Any(e => e.Url == DataAcquisitionConstants.Extension.DateReceivedExtensionUri))
+        if (!resource.Meta.Extension.Any(e => e.Url == DataAcquisitionConstants.Extension.DateReceivedExtensionUri))
             resource.Meta.Extension.Add(new Extension { Url = DataAcquisitionConstants.Extension.DateReceivedExtensionUri, Value =  new FhirDateTime(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))});
     }
     #endregion
