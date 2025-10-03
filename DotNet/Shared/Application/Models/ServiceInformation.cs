a61aa77c8de23feb6853b846bcc28157a9bf2308
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2025-10-03 10:18:32 -0700

    Having the Admin BFF respond with an array of service info for each of the services in the service registry.

--- DotNet/Shared/Application/Models/ServiceInformation.cs
+++ DotNet/Shared/Application/Models/ServiceInformation.cs
@@ -1,10 +1,15 @@
 ﻿using System.Reflection;
 using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
 
 namespace LantanaGroup.Link.Shared.Application.Models;
 
 public class ServiceInformation
 {
+    private static readonly ILogger _logger;
+
     public static string SectionName = "ServiceInformation";
     public string ServiceName { get; init; } = string.Empty;
     public string Version { get; set; } = string.Empty;
@@ -23,5 +28,33 @@ public class ServiceInformation
             serviceInformation.Version = assemblyVersion;
 
         return serviceInformation;
+    }
+
+    /**
+     * Gets service information for a given service at its base URL.
+     */
+    public static async Task<ServiceInformation?> GetServiceInformation(HttpClient client, string? serviceInfoEndpoint, ILogger logger)
+    {
+        if (string.IsNullOrEmpty(serviceInfoEndpoint))
+            return null;
+        
+        try
+        {
+            var response = await client.GetAsync(serviceInfoEndpoint);
+            if (!response.IsSuccessStatusCode)
+            {
+                logger.LogWarning("Failed to retrieve service information from endpoint {Endpoint} due to status code {StatusCode}", serviceInfoEndpoint, response.StatusCode);
+                return null;
+            }
+            var content = await response.Content.ReadAsStringAsync();
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.PropertyNameCaseInsensitive = true;
+            return JsonSerializer.Deserialize<ServiceInformation>(content, options);
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning("Failed to retrieve service information from endpoint {Endpoint}: {Message}", serviceInfoEndpoint, ex.Message);
+            return null;
+        }
     }
 }
