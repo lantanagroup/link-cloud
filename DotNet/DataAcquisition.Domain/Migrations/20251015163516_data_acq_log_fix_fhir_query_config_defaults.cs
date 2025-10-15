122961cb3ea5a13f62f61d6d064d0781f3fcc793
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-10-15 11:41:13 -0500

    LNK-4321-Correct-invalid-fhir-query-configs

---
+++ DotNet/DataAcquisition.Domain/Migrations/20251015163516_data_acq_log_fix_fhir_query_config_defaults.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcquisition.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class data_acq_log_fix_fhir_query_config_defaults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE FhirQueryConfiguration SET MinAcquisitionPullTime = NULL, MaxAcquisitionPullTime = NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE FhirQueryConfiguration SET MinAcquisitionPullTime = '00:00:00', MaxAcquisitionPullTime = '00:00:00'");
+        }
+    }
+}
