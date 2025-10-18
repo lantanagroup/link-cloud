fd2947b07d6ad713bd6c43d12dcf0695465f576a
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2025-10-17 23:21:56 -0500

    Update ResourceAcquired.cs

--- DotNet/DataAcquisition.Domain/Application/Models/Kafka/ResourceAcquired.cs
+++ DotNet/DataAcquisition.Domain/Application/Models/Kafka/ResourceAcquired.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.Json;
-using Hl7.Fhir.Model;
-using Hl7.Fhir.Serialization;
+﻿using Hl7.Fhir.Model;
 using LantanaGroup.Link.Shared.Application.Models;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Kafka;
@@ -13,18 +11,4 @@ public class ResourceAcquired
     public Resource Resource { get; set; }
     public List<ScheduledReport> ScheduledReports { get; set; } = new List<ScheduledReport>();
     public ReportableEvent ReportableEvent { get; set; }
-
-    // Serialize this object to a string for safe JobDataMap storage (FHIR-compliant)
-    public string ToFhirJson()
-    {
-        var options = new JsonSerializerOptions().ForFhir(ModelInfo.ModelInspector);
-        return JsonSerializer.Serialize(this, options);
-    }
-
-    // Deserialize from string (FHIR-compliant)
-    public static ResourceAcquired FromFhirJson(string json)
-    {
-        var options = new JsonSerializerOptions().ForFhir(ModelInfo.ModelInspector);
-        return JsonSerializer.Deserialize<ResourceAcquired>(json, options);
-    }
 }
