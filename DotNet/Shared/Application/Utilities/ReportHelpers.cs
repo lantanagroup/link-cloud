14f349f2c6ff171ee621fc9eb60e666b60c29b75
Steven Williams <steven.williams@lantanagroup.com>
2025-10-23 11:33:56 -0400

    Use unhashed ID in report name

--- DotNet/Shared/Application/Utilities/ReportHelpers.cs
+++ DotNet/Shared/Application/Utilities/ReportHelpers.cs
@@ -8,10 +8,6 @@
             if (string.IsNullOrEmpty(facilityId)) throw new ArgumentException("Facility ID cannot be null or empty.", nameof(facilityId));
             if (reportTypes == null || reportTypes.Count == 0) throw new ArgumentException("Report types cannot be null or empty.", nameof(reportTypes));
 
-            long hash = scheduleID.GetStableHashCode64();
-            byte[] hashBytes = BitConverter.GetBytes(hash);
-            string hashString = Convert.ToBase64String(hashBytes).TrimEnd('=').Replace("+", "-").Replace("/", "_");
-
             List<string> parts = [
                 facilityId.ToLowerInvariant(),
                 string.Join('+', reportTypes.Select(rt => MeasureNameShortener.ShortenMeasureName(rt).ToLowerInvariant()).Order())
@@ -20,7 +16,7 @@
             {
                 parts.Add(reportStartDate.Value.ToString("yyyyMMdd"));
             }
-            parts.Add(hashString);
+            parts.Add(scheduleID.ToLowerInvariant());
             return string.Join('_', parts);
         }
     }
