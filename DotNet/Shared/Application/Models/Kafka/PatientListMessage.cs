f2407ecc63855968c2da58b6b18c9164975e3a12
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2025-11-04 16:21:47 -0500

    LNK-3956: Upgrade Integration Test for POI changes

---
+++ DotNet/Shared/Application/Models/Kafka/PatientListMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LantanaGroup.Link.Shared.Application.Models.Kafka
+{
+    public class PatientListMessage
+    {
+        public string ReportTrackingId { get; set; } = string.Empty;
+        public List<PatientListItem> PatientLists { get; set; } = new();
+    }
+}
