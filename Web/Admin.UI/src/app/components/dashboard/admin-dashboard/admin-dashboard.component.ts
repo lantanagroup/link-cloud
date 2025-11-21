c520221ef14f29825dd97ca4b0da96711baf86a5
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2025-11-21 09:45:06 -0500

    LNK-4524: added auth guard to prevent unauthorized access to different routes

--- Web/Admin.UI/src/app/components/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ Web/Admin.UI/src/app/components/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { UserProfileService } from '../../../services/user-profile.service';
 
-
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
@@ -16,12 +15,10 @@ export class AdminDashboardComponent implements OnInit {
   }
 
   async ngOnInit(): Promise<void> {
+
     try {
-      await this.userProfileService.fetchAndSetProfile();
       const profile  = await this.userProfileService.getProfile();
-      if (profile?.firstName && profile?.lastName) {
-        this.name = profile.firstName + ' ' + profile.lastName;
-      }
+      this.name = profile?.firstName && profile?.lastName ? `${profile.firstName} ${profile.lastName}` : '';
     }
     catch (error) {
       console.error('Failed to fetch user profile:', error);
