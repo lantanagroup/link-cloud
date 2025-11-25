19db0523e4a9bd5e5682ecad3d1218fe2eb006b8
MontaltoNick <nick.montalto@lantanagroup.com>
2025-11-25 09:24:33 -0600

    checkin

---
+++ DotNet/DataAcquisition.Domain/Migrations/20251124162057_FixIndex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcquisition.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIndexMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_DataAcquisitionLogs_Paging_Default", table: "DataAcquisitionLog");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataAcquisitionLogs_Paging_Default",
+                table: "DataAcquisitionLog",
+                columns: new[] { "ExecutionDate", "Id" },
+                descending: new bool[0])
+                .Annotation("SqlServer:Include", new[] { "Priority", "FacilityId", "IsCensus", "PatientId", "ReportableEvent", "ReportTrackingId", "CorrelationId", "TraceId", "FhirVersion", "QueryType", "QueryPhase", "Status", "RetryAttempts", "CompletionDate", "CompletionTimeMilliseconds" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DataAcquisitionLogs_Paging_Default",
+                table: "DataAcquisitionLog",
+                columns: new[] { "ExecutionDate", "Id" },
+                descending: new bool[0])
+                .Annotation("SqlServer:Include", new[] { "Priority", "FacilityId", "IsCensus", "PatientId", "ReportableEvent", "ReportTrackingId", "CorrelationId", "TraceId", "FhirVersion", "QueryType", "QueryPhase", "Status", "RetryAttempts", "CompletionDate", "CompletionTimeMilliseconds, ResourceId" });
+        }
+    }
+}
