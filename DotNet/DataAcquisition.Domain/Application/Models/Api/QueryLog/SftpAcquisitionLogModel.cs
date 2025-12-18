e19e3e021937dbfc33f36fe54b22215729f70ff7
Adam Phillips <adam.phillips@lantanagroup.com>
2025-12-18 09:05:34 -0500

    added new sftp acquisition log entity and model

---
+++ DotNet/DataAcquisition.Domain/Application/Models/Api/QueryLog/SftpAcquisitionLogModel.cs
@@ -0,0 +1,38 @@
+using LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Entities;
+
+namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Api.QueryLog;
+
+public record SftpAcquisitionLogModel(
+    long Id, 
+    Guid? ExternalId, 
+    string FacilityId, 
+    string FacilityName, 
+    int PatientCount, 
+    int EncounterCount, 
+    DateTime? ProcessDate);
+
+
+public static class SftpAcquisitionLogModelExtensions
+{
+
+    public static SftpAcquisitionLogModel ToModel(this SftpAcquisitionLog entity) => new(
+        entity.Id,
+        entity.ExternalId,
+        entity.FacilityId,
+        entity.FacilityName,
+        entity.PatientCount,
+        entity.EncounterCount,
+        entity.ProcessDate
+    );
+    
+    public static SftpAcquisitionLog ToDomain(this SftpAcquisitionLogModel model) => new()
+    {
+        ExternalId = model.ExternalId ?? Guid.NewGuid(),
+        FacilityId = model.FacilityId,
+        FacilityName = model.FacilityName,
+        PatientCount = model.PatientCount,
+        EncounterCount = model.EncounterCount,
+        ProcessDate = model.ProcessDate ?? DateTime.UtcNow
+    };
+
+}
---
+++ DotNet/DataAcquisition.Domain/Infrastructure/Entities/SftpAcquisitionLog.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Entities;
+
+[Table("SftpAcquisitionLog")]
+public class SftpAcquisitionLog
+{
+    [Key]
+    public long Id { get; set; }
+    
+    public Guid ExternalId { get; set; }
+    
+    public string FacilityId { get; set; } = string.Empty;
+    
+    public string FacilityName { get; set; } = string.Empty;
+    
+    public int PatientCount { get; set; }
+    
+    public int EncounterCount { get; set; }
+    
+    public DateTime ProcessDate { get; set; } = DateTime.UtcNow;
+}
