518f7c0ac2e0f934f313804e24b1bb47ef5e97be
MontaltoNick <nick.montalto@lantanagroup.com>
2025-12-22 15:06:15 -0600

    checkin

--- DotNet/Report/Domain/MongoIndexCreationService.cs
+++ DotNet/Report/Domain/MongoIndexCreationService.cs
@@ -9,7 +9,7 @@ namespace LantanaGroup.Link.Report.Domain
         private readonly IServiceScopeFactory _serviceScopeFactory;
         public MongoIndexCreationService(ILogger<MongoIndexCreationService> logger, IServiceScopeFactory factory) 
         {
-
+            _logger = logger;
             _serviceScopeFactory = factory;
         }
 
