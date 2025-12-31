0cf2dfb935c2aa5ef9caca081ae2fe862b471eae
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2025-12-31 15:52:49 -0500

    LNK-4651: Add AddHoc Type

---
+++ DotNet/Shared/Application/Models/AdHocType.cs
@@ -0,0 +1,13 @@
+﻿using LantanaGroup.Link.Shared.Application.Utilities;
+using System.Text.Json.Serialization;
+
+namespace LantanaGroup.Link.Shared.Application.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum AdHocType
+{
+    [StringValue("Manual")]
+    Manual = 0,
+    [StringValue("Automatic")]
+    Automatic = 1
+}
