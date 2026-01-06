1782e19f4e8532e95625c80ac6c02d10be2ee31d
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2026-01-06 09:59:54 -0600

    Update MeasureEval for LinkAdmin
    * check for LinkAdministrator role.

--- Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/controllers/PatientController.java
+++ Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/controllers/PatientController.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/measureeval/patient")
-@PreAuthorize("hasRole('LinkUser')")
+@PreAuthorize("hasAnyRole('LinkUser','LinkAdministrator')")
 public class PatientController {
     private final PatientReportingEvaluationStatusRepository patientReportingEvaluationStatusRepository;
     private final PatientStatusBundler patientStatusBundler;
