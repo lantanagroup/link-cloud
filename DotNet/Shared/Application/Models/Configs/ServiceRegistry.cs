377f47ca111f5c2bad2145da4780f820c33a7eda
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2026-01-07 07:35:01 -0800

    Revert "Allowing ServiceRegistry's PublicXXXServiceUrl properties to fall back to XXXServiceUrl if the Public URL has not been defined."
    This reverts commit 2dffd706241fb041a1db0c21eb71b7acc2c2b826.

--- DotNet/Shared/Application/Models/Configs/ServiceRegistry.cs
+++ DotNet/Shared/Application/Models/Configs/ServiceRegistry.cs
@@ -5,103 +5,32 @@ namespace LantanaGroup.Link.Shared.Application.Models.Configs
     public class ServiceRegistry
     {
         public static string ConfigSectionName = "ServiceRegistry";
+
         public string AccountServiceUrl { get; set; } = null!;
-        private string? _publicAccountServiceUrl;
-        public string? PublicAccountServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicAccountServiceUrl) ? AccountServiceUrl : _publicAccountServiceUrl;
-            set => _publicAccountServiceUrl = value;
-        }
-        
+        public string? PublicAccountServiceUrl { get; set; }
         public string AuditServiceUrl { get; set; } = null!;
-        private string? _publicAuditServiceUrl;
-        public string? PublicAuditServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicAuditServiceUrl) ? AuditServiceUrl : _publicAuditServiceUrl;
-            set => _publicAuditServiceUrl = value;
-        }
-        
+        public string? PublicAuditServiceUrl { get; set; }
         public string CensusServiceUrl { get; set; } = null!;
-        private string? _publicCensusServiceUrl;
-        public string? PublicCensusServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicCensusServiceUrl) ? CensusServiceUrl : _publicCensusServiceUrl;
-            set => _publicCensusServiceUrl = value;
-        }
-        
+        public string? PublicCensusServiceUrl { get; set; }
         public string DataAcquisitionServiceUrl { get; set; } = null!;
-        private string? _publicDataAcquisitionServiceUrl;
-        public string? PublicDataAcquisitionServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicDataAcquisitionServiceUrl) ? DataAcquisitionServiceUrl : _publicDataAcquisitionServiceUrl;
-            set => _publicDataAcquisitionServiceUrl = value;
-        }
-        
+        public string? PublicDataAcquisitionServiceUrl { get; set; }
         public string MeasureServiceUrl { get; set; } = null!;
-        private string? _publicMeasureServiceUrl;
-        public string? PublicMeasureServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicMeasureServiceUrl) ? MeasureServiceUrl : _publicMeasureServiceUrl;
-            set => _publicMeasureServiceUrl = value;
-        }
-        
+        public string? PublicMeasureServiceUrl { get; set; }
         public string NormalizationServiceUrl { get; set; } = null!;
-        private string? _publicNormalizationServiceUrl;
-        public string? PublicNormalizationServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicNormalizationServiceUrl) ? NormalizationServiceUrl : _publicNormalizationServiceUrl;
-            set => _publicNormalizationServiceUrl = value;
-        }
-        
+        public string? PublicNormalizationServiceUrl { get; set; }
         public string NotificationServiceUrl { get; set; } = null!;
-        private string? _publicNotificationServiceUrl;
-        public string? PublicNotificationServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicNotificationServiceUrl) ? NotificationServiceUrl : _publicNotificationServiceUrl;
-            set => _publicNotificationServiceUrl = value;
-        }
-        
+        public string? PublicNotificationServiceUrl { get; set; }
         public string QueryDispatchServiceUrl { get; set; } = null!;
-        private string? _publicQueryDispatchServiceUrl;
-        public string? PublicQueryDispatchServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicQueryDispatchServiceUrl) ? QueryDispatchServiceUrl : _publicQueryDispatchServiceUrl;
-            set => _publicQueryDispatchServiceUrl = value;
-        }
-        
+        public string? PublicQueryDispatchServiceUrl { get; set; }
         public string ReportServiceUrl { get; set; } = null!;
-        private string? _publicReportServiceUrl;
-        public string? PublicReportServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicReportServiceUrl) ? ReportServiceUrl : _publicReportServiceUrl;
-            set => _publicReportServiceUrl = value;
-        }
-        
+        public string? PublicReportServiceUrl { get; set; }
         public string SubmissionServiceUrl { get; set; } = null!;
-        private string? _publicSubmissionServiceUrl;
-        public string? PublicSubmissionServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicSubmissionServiceUrl) ? SubmissionServiceUrl : _publicSubmissionServiceUrl;
-            set => _publicSubmissionServiceUrl = value;
-        }
-        
+        public string? PublicSubmissionServiceUrl { get; set; }
         public string ValidationServiceUrl { get; set; } = null!;
-        private string? _publicValidationServiceUrl;
-        public string? PublicValidationServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicValidationServiceUrl) ? ValidationServiceUrl : _publicValidationServiceUrl;
-            set => _publicValidationServiceUrl = value;
-        }
-        
+        public string? PublicValidationServiceUrl { get; set; }
         public TenantServiceRegistration TenantService { get; set; } = null!;
-        
         public string TerminologyServiceUrl { get; set; } = null!;
-        private string? _publicTerminologyServiceUrl;
-        public string? PublicTerminologyServiceUrl 
-        { 
-            get => string.IsNullOrEmpty(_publicTerminologyServiceUrl) ? TerminologyServiceUrl : _publicTerminologyServiceUrl;
-            set => _publicTerminologyServiceUrl = value;
-        }
+        public string? PublicTerminologyServiceUrl { get; set; }
 
         public string? TerminologyServiceApiUrl
         {
