f5fbf1606f2b1184fe7058ab2a9097cdad0c4b35
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2026-01-09 08:59:56 -0800

    Forcing ABS nuget package to verseion 12.26 since 12.27 has some breaking changes for our use. Adding health check to submission service for the database. Ensuring that the Submission.Data project gets included in the "restore" phase of Docker build for submission.

---
+++ DotNet/Submission/Application/HealthChecks/DatabaseHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using LantanaGroup.Link.Shared.Application.Models;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Submission.Data;
+
+namespace LantanaGroup.Link.Submission.Application.HealthChecks;
+
+public class DatabaseHealthCheck(SubmissionContext context) : IHealthCheck
+{
+    private readonly SubmissionContext _context = context ?? throw new ArgumentNullException(nameof(context));
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var canConnect = await _context.Database.CanConnectAsync(cancellationToken);
+            return canConnect 
+                ? HealthCheckResult.Healthy(nameof(HealthCheckType.Database)) 
+                : HealthCheckResult.Unhealthy(nameof(HealthCheckType.Database));
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy(exception: ex);
+        }
+    }
+}
