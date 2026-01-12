439e5b215749e4703622747b49df9a3a6541a16b
Adam Phillips <adam.phillips@lantanagroupcom>
2026-01-12 08:54:46 -0500

    added sftp configuration entity and model

---
+++ DotNet/DataAcquisition.Domain/Application/Models/Api/Configuration/SftpConfigurationModel.cs
@@ -0,0 +1,47 @@
+﻿using LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Entities;
+using LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Models;
+
+namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Api.Configuration;
+
+public class SftpConfigurationModel
+{
+    public Guid Id { get; set; }
+    public string Host { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public string? RemoteDirectory { get; set; }
+    public TimeSpan Timeout { get; set; }
+    public bool RemoveAfterProcessing { get; set; }
+    public AuthType AuthenticationProtocol { get; set; }
+}
+
+public static class SftpConfigurationModelExtensions
+{
+    public static SftpConfigurationModel ToModel(this Infrastructure.Entities.SftpConfiguration entity)
+    {
+        return new SftpConfigurationModel
+        {
+            Id = entity.Id,
+            Host = entity.Host,
+            Port = entity.Port,
+            RemoteDirectory = entity.RemoteDirectory,
+            Timeout = entity.Timeout,
+            RemoveAfterProcessing = entity.RemoveAfterProcessing,
+            AuthenticationProtocol = entity.AuthenticationProtocol
+        };
+    }
+    
+    public static SftpConfiguration ToEntity(this SftpConfigurationModel model, string organizationId)
+    {
+        return new SftpConfiguration
+        {
+            Id = model.Id,
+            OrganizationId = organizationId,
+            Host = model.Host,
+            Port = model.Port,
+            RemoteDirectory = model.RemoteDirectory,
+            Timeout = model.Timeout,
+            RemoveAfterProcessing = model.RemoveAfterProcessing,
+            AuthenticationProtocol = model.AuthenticationProtocol
+        };
+    }
+}
