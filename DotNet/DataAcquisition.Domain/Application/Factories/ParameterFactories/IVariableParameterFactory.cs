0d28e017c100dfb743134fdfb3496bffffead86c
Sean McIlvenna <seanmcilvenna@users.noreply.github.com>
2026-01-12 18:19:20 -0800

    LNK-4705: Resource queries not populated with all parameters (#1368)
    * Parameter handling enhancements
    - ParameterQueryFactory handles paged parameters better.
    - ResourceIdParameterFactory queries for resourceIds from the DB before constructing the query.
    
    * Adding unit tests to ParameterQueryFactory and ResourceIdParameterFactory to confirm correct handling of paging scenarios
    
    * Turning parameter factories into services that can use dependency injection to create loggers.
    
    * Using TryParse for safer resource type parsing
    
    * Removing unnecessary try/catch
    
    * Validating _dataAcqusitionLogQueries in the constructor
    
    * Fixing typo

---
+++ DotNet/DataAcquisition.Domain/Application/Factories/ParameterFactories/IVariableParameterFactory.cs
@@ -0,0 +1,11 @@
+﻿using LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Api.Requests;
+using LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Factory.ParameterQuery;
+using LantanaGroup.Link.DataAcquisition.Domain.Infrastructure.Models.QueryConfig.Parameter;
+using LantanaGroup.Link.Shared.Application.Models;
+
+namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Factories.ParameterFactories;
+
+public interface IVariableParameterFactory
+{
+    ParameterFactoryResult? Build(VariableParameter parameter, GetPatientDataRequest request, ScheduledReport scheduledReport, string lookback);
+}
