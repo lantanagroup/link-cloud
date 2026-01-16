ced8a12f9c58e6cb34c5cc487b074364ce447f08
Steven Williams <steven.williams@lantanagroup.com>
2026-01-16 14:25:34 -0500

    Add timeouts and explicit disconnection

--- Java/hapi/src/main/java/com/lantanagroup/link/hapi/HealthCheck.java
+++ Java/hapi/src/main/java/com/lantanagroup/link/hapi/HealthCheck.java
@@ -4,14 +4,23 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HealthCheck {
+    private static final int TIMEOUT = 10000;
+
     public static void main(String[] args) {
         try {
             URL url = new URL(args[0]);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            int responseCode = connection.getResponseCode();
-            System.out.println(responseCode);
-            if (responseCode != HttpURLConnection.HTTP_OK) {
-                fail();
+            connection.setConnectTimeout(TIMEOUT);
+            connection.setReadTimeout(TIMEOUT);
+            connection.connect();
+            try {
+                int responseCode = connection.getResponseCode();
+                System.out.println(responseCode);
+                if (responseCode != HttpURLConnection.HTTP_OK) {
+                    fail();
+                }
+            } finally {
+                connection.disconnect();
             }
         } catch (Exception e) {
             System.err.println(e);
