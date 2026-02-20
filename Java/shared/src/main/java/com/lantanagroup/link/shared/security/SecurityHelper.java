068ebb8c8a1b28e2fa966ea5c621043e4ad49113
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2026-02-20 09:08:41 -0800

    Permit unauthenticated GET access to /v3/api-docs/** and /swagger-ui.html endpoints in security config

--- Java/shared/src/main/java/com/lantanagroup/link/shared/security/SecurityHelper.java
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/security/SecurityHelper.java
@@ -49,6 +49,8 @@ public class SecurityHelper {
             .requestMatchers(HttpMethod.GET, "/info").permitAll()
             .requestMatchers(HttpMethod.GET, "/api/*/info").permitAll()
             .requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
             .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
             //requestMatchers("/api/**").access("hasRole('LinkUser') and hasAuthority('IsLinkAdmin')").and().exceptionHandling(ex -> ex.authenticationEntryPoint(point)  - done in the specific end points using annotations for more granular control
             .requestMatchers("/api/**").authenticated().and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
