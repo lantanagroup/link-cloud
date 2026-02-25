71578e32c96de8e1e72901486ca2f1c5bad6b709
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2026-02-25 12:46:36 -0800

    Refactor Kafka integration to replace `string` keys with `ResourceKey` in producer and consumer configurations for `ResourceAcquired` and `ResourceNormalized` events produced/consumed between DataAcquisition, Normalization and MeasureEval

---
+++ DotNet/Shared/Application/Models/Kafka/ResourceKey.cs
@@ -0,0 +1,7 @@
+﻿namespace LantanaGroup.Link.Shared.Application.Models.Kafka;
+
+public class ResourceKey
+{
+    public string FacilityId { get; set; } = string.Empty;
+    public string CorrelationId { get; set; } = string.Empty;
+}
---
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/kafka/records/ResourceKey.java
@@ -0,0 +1,15 @@
+package com.lantanagroup.link.shared.kafka.records;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResourceKey {
+    private String facilityId;
+    private String correlationId;
+}
