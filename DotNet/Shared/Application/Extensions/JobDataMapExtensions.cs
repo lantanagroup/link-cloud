5aea74652fe01a357f8cf8d267019e99ea858b7e
MontaltoNick <nick.montalto@lantanagroup.com>
2026-03-09 10:17:14 -0500

    checkin

--- DotNet/Shared/Application/Extensions/JobDataMapExtensions.cs
+++ DotNet/Shared/Application/Extensions/JobDataMapExtensions.cs
@@ -44,10 +44,22 @@ public static class JobDataMapExtensions
         {
             if (storedValue is string rawValue && !string.IsNullOrEmpty(rawValue))
             {
-                string? deserialized = JsonSerializer.Deserialize<string>(rawValue);
-                if (deserialized != null)
-                    return (T)(object)deserialized;
+                try
+                {
+                    string? deserialized = JsonSerializer.Deserialize<string>(rawValue);
+                    if (deserialized != null)
+                        return (T)(object)deserialized;
+                }
+                catch
+                {
+                }
 
+                string result = rawValue;
+                if (result.Length >= 2 && result[0] == '"' && result[^1] == '"')
+                {
+                    result = result[1..^1];
+                }
+                return (T)(object)result;
             }
             return default;
         }
