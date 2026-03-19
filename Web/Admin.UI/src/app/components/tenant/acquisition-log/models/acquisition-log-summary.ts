c6d0a18483838c564d1d8810964211dd4c23be07
Ariana D Mihailescu <82962995+arianamihailescu@users.noreply.github.com>
2026-03-19 10:45:28 -0400

    LNK-3711: Code Rabittai suggestions

--- Web/Admin.UI/src/app/components/tenant/acquisition-log/models/acquisition-log-summary.ts
+++ Web/Admin.UI/src/app/components/tenant/acquisition-log/models/acquisition-log-summary.ts
@@ -15,7 +15,7 @@ export interface AcquisitionLogSummary {
     completionDate?: Date;
     retryAttempts: number;
     status: string;
-    reportTrackingId: string;
+    reportTrackingId?: string | null;
     reportIds: string[];
     isDeleted: boolean;
 }
