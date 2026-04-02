56b7a3fa5a149422159febc829cef537a0ca4094
Daniel Vargas <116004423+dvargaslantana@users.noreply.github.com>
2026-04-01 23:20:18 -0500

    Removed commented code

--- DotNet/Normalization/Program.cs
+++ DotNet/Normalization/Program.cs
@@ -196,17 +196,10 @@ static void RegisterServices(WebApplicationBuilder builder)
     builder.Services.AddTransient<RetryJob>();
 
     builder.Services.AddSingleton<CopyPropertyOperationService>();
-    //builder.Services.AddHostedService(provider => provider.GetRequiredService<CopyPropertyOperationService>());
-
     builder.Services.AddSingleton<CodeMapOperationService>();
-    //builder.Services.AddHostedService(provider => provider.GetRequiredService<CodeMapOperationService>());
-
     builder.Services.AddSingleton<ConditionalTransformOperationService>();
-    //builder.Services.AddHostedService(provider => provider.GetRequiredService<ConditionalTransformOperationService>());
-
     builder.Services.AddSingleton<CopyLocationOperationService>();
-    //builder.Services.AddHostedService(provider => provider.GetRequiredService<CopyLocationOperationService>());
-
+    
     if (consumerSettings != null && !consumerSettings.DisableConsumer)
     {
          builder.Services.AddHostedService<ResourceAcquiredListener>();
