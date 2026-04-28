523eeb795b044c35336f0484b5016ff97e83912a
John Britton <johnbritton@users.noreply.github.com>
2026-04-28 08:09:11 -0400

    Change CustomHeaderAuth to use secrets for header values

--- DotNet/DataAcquisition.Domain/Application/Services/Auth/CustomHeaderAuth.cs
+++ DotNet/DataAcquisition.Domain/Application/Services/Auth/CustomHeaderAuth.cs
@@ -1,17 +1,33 @@
 using DataAcquisition.Domain.Application.Models;
 using LantanaGroup.Link.DataAcquisition.Domain.Application.Services.Interfaces;
+using LantanaGroup.Link.Shared.Application.Interfaces.Services;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Services.Auth;
 
 public class CustomHeaderAuth : IAuth
 {
-    public Task<(bool isQueryParam, object authHeaderValue)> SetAuthentication(string facilityId, AuthenticationConfigurationModel authSettings)
+    private readonly ISecretManager _secretManager;
+
+    public CustomHeaderAuth(ISecretManager secretManager)
+    {
+        _secretManager = secretManager;
+    }
+
+    public async Task<(bool isQueryParam, object authHeaderValue)> SetAuthentication(string facilityId, AuthenticationConfigurationModel authSettings)
     {
         if (authSettings.CustomHeaders == null || !authSettings.CustomHeaders.Any())
         {
-            return Task.FromResult<(bool, object)>((false, null));
+            return (false, new Dictionary<string, string>());
         }
 
-        return Task.FromResult<(bool, object)>((false, authSettings.CustomHeaders));
+        var headers = new Dictionary<string, string>();
+        foreach (var header in authSettings.CustomHeaders)
+        {
+            var secretName = header.Value;
+            var secretValue = await _secretManager.GetSecretAsync(secretName, CancellationToken.None);
+            headers.Add(header.Key, secretValue ?? "");
+        }
+
+        return (false, headers);
     }
 }
