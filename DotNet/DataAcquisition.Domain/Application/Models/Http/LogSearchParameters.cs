29e7c4fe2a7e1463eab9db67fd383ce48b5f0502
Steven Williams <steven.williams@lantanagroup.com>
2026-05-03 12:17:53 -0400

    Remediate Mass Assignment: Insecure Binder Configuration

--- DotNet/DataAcquisition.Domain/Application/Models/Http/LogSearchParameters.cs
+++ DotNet/DataAcquisition.Domain/Application/Models/Http/LogSearchParameters.cs
@@ -5,38 +5,77 @@ using LantanaGroup.Link.Shared.Application.Models.Integration.DataAcquisition;
 using RequestStatus = LantanaGroup.Link.Shared.Application.Models.Integration.DataAcquisition.RequestStatus;
 using QueryPhase = LantanaGroup.Link.Shared.Application.Models.Integration.DataAcquisition.QueryPhase;
 using FhirQueryType = LantanaGroup.Link.Shared.Application.Models.Integration.DataAcquisition.FhirQueryType;
+using System.Runtime.Serialization;
 
 namespace LantanaGroup.Link.DataAcquisition.Domain.Application.Models.Http;
 
+[DataContract]
 public class LogSearchParameters : GenericLogSearchParameters
 {
+    [DataMember]
     public string? FacilityId { get; set; }
+
+    [DataMember]
     public string? PatientId { get; set; }
+
+    [DataMember]
     public string? ReportId { get; set; }
+
+    [DataMember]
     public string? ResourceId { get; set; }
+
+    [DataMember]
     public QueryPhase? QueryPhase { get; set; }
+
+    [DataMember]
     public FhirQueryType? QueryType { get; set; }
+
+    [DataMember]
     public List<RequestStatus>? Statuses { get; set; }
+
+    [DataMember]
     public AcquisitionPriority? Priority { get; set; }
+
+    [DataMember]
     public string? ResourceType { get; set; }
+
+    [DataMember]
     public DateTime? CreatedBefore { get; set; }
 }
 
+[DataContract]
 public class SftpLogSearchParameters : GenericLogSearchParameters
 {
+    [DataMember]
     public string? FacilityId { get; set; }
+
+    [DataMember]
     public RequestStatus? Status { get; set; }
+
+    [DataMember]
     public SftpAcquisitionType? AcquisitionType { get; set; }
+
+    [DataMember]
     public SftpAcquisitionSubType? SubType { get; set; }
 }
 
+[DataContract]
 public class GenericLogSearchParameters
 {
+    [DataMember]
     [Range(1, int.MaxValue, ErrorMessage = "PageNumber must be greater than 0")]
     public int PageNumber { get; set; } = 1;
+
+    [DataMember]
     [Range(1, 100, ErrorMessage = "PageSize must be between 1 and 100")]
     public int PageSize { get; set; } = 10;
+
+    [DataMember]
     public string SortBy { get; set; } = "ExecutionDate";
+
+    [DataMember]
     public SortOrder SortOrder { get; set; } = SortOrder.Descending;
+
+    [DataMember]
     public bool IncludeDeleted { get; set; } = false;
 }
