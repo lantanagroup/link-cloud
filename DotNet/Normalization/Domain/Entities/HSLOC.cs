231d17c6e3c5bf1a76454e83c0c0bf6b2befd83f
John Britton <johnbritton@users.noreply.github.com>
2026-08-20 09:23:13 -0400

    Add HSLOC entity with migration and seed data

---
+++ DotNet/Normalization/Domain/Entities/HSLOC.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LantanaGroup.Link.Normalization.Domain.Entities;
+
+[Table("HSLOC")]
+public partial class HSLOC
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string HSLOCCode {get; set; }
+    public string CDCCode {get; set; }
+    public string ShortDescription {get; set; }
+    public string LongDescription {get; set; }
+    public string Version {get;set;}
+    
+}
