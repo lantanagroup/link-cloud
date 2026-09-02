0ab87ab14f23b046ae89a6ec09867151e3a834a0
John Britton <johnbritton@users.noreply.github.com>
2026-09-02 12:51:57 -0400

    LEGLINK-843: Static scan fixes, docs update, and version numbers update (#1890)
    * Fix privacy issues from static scan in EpicAuth.cs
    
    * Store values as char array to minimize window for heap inspection
    
    * Add DataContract annotations to models
    
    * Fix failing tests and bug when calling ToString on a char[]
    
    * Fix bug where signing credentials were disposed of too early
    
    * Update version numbers to 0.7.1
    
    * Sync /docs with latest

--- DotNet/Normalization/Application/Models/Operations/HttpModels/VendorVersionOperationPresetPostModel.cs
+++ DotNet/Normalization/Application/Models/Operations/HttpModels/VendorVersionOperationPresetPostModel.cs
@@ -1,14 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
 namespace LantanaGroup.Link.Normalization.Application.Models.Operations.HttpModels;
 
 [ExcludeFromCodeCoverage]
+[DataContract]
 public class VendorVersionOperationPresetPostModel
 {
     [Required]
+    [DataMember]
     public Guid? VendorVersionId { get; set; }
 
     [Required]
+    [DataMember]
     public Guid? OperationResourceTypeId { get; set; }
 }
